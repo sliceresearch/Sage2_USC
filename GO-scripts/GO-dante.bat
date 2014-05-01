c5899f8c956ef9026fe3ed0e263de8c54d90036b
tmarrinan <tmarri2@uic.edu>
2014-05-01 14:39:10 -0500

    updated dante (windows)
    Former-commit-id: b0dea3df844810914bb041753ac4a906d366149a

--- GO-scripts/GO-dante.bat
+++ GO-scripts/GO-dante.bat
@@ -1,2 +1,3 @@
-cd C:\Users\SAGE2\Desktop\sage2
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --window-size=4098,2304 --window-position=-8,-1536 --app=https://dante.evl.uic.edu:9090/?clientID=0
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --window-size=4098,2304 --window-position=-8,-1536 --app=https://localhost:9090/audioManager.html /B
+timeout 1
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --window-size=4098,2304 --window-position=-8,-1536 --app=https://localhost:9090/?clientID=0 /B
