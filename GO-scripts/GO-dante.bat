da2bedb23c8445dba1b7d63678c9706b9f99a28a
tmarrinan <tmarri2@uic.edu>
2014-05-01 14:03:11 -0500

    added windows config and GO script
    Former-commit-id: 23bf0621f9758cf120c571f7a6cfd11194bfeacd

---
+++ GO-scripts/GO-dante.bat
@@ -0,0 +1,2 @@
+cd C:\Users\SAGE2\Desktop\sage2
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --window-size=4098,2304 --window-position=-8,-1536 --app=https://dante.evl.uic.edu:9090/?clientID=0
