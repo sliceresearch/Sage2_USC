1775d3c1ea2d7c75c9009c3aa5494196c2c245a4
Andy J <ajohnson@uic.edu>
2014-08-29 17:05:21 -0500

    sage2 version of andys cave 2 monitor
    Former-commit-id: ba303cd36282de8730d6f16863a37540a7eaa762

---
+++ public_HTTPS/uploads/apps/cave2_monitor/cave2_monitor.js
@@ -0,0 +1,541 @@
+// SAGE2 is available for use under the SAGE2 Software License
+//
+// University of Illinois at Chicago's Electronic Visualization Laboratory (EVL)
+// and University of Hawai'i at Manoa's Laboratory for Advanced Visualization and
+// Applications (LAVA)
+//
+// See full text, terms and conditions in the LICENSE.txt included file
+//
+// Copyright (c) 2014
+
+////////////////////////////////////////
+// cave2 status viewer
+// Written by Andy Johnson - 2005 - 2014
+// now in D3 / SVG / etc
+////////////////////////////////////////
+
+
+var cave2_monitor = SAGE2_App.extend( {
+
+    construct: function() {
+        arguments.callee.superClass.construct.call(this);
+
+        this.resizeEvents = "continuous"; //onfinish
+        this.svg = null;
+
+
+    this.gwin = {};
+    this.myTag = "";
+  
+    this.gwin.sampleSVG = null;
+
+    this.gwin.date = "Loading ...";
+    this.gwin.hour = "" ;
+    this.gwin.ampm = "";
+    this.gwin.outside = "NULL";
+    this.gwin.roughDate = "";
+
+    this.gwin.displayFont = "Arial";
+    this.gwin.massiveFontSize = "80px";
+    this.gwin.largeFontSize = "36px";
+    this.gwin.smallFontSize = "12px";
+
+    this.gwin.canvasBackground = "black";
+
+    this.gwin.rounded = 4; 
+
+    this.gwin.margin = 20;
+    this.gwin.canvasHeight = 630;
+    this.gwin.canvasWidth = 1260;
+
+    this.gwin.weatherIcon = "";
+    this.gwin.weatherImage = new Image();
+    this.gwin.iconSet = "";
+
+    this.gwin.conditions = "";
+
+    this.glob = {};
+    this.glob.perc1 = 1;
+    this.glob.perc2 = 1;
+    this.glob.perc3 = 1;
+    this.glob.perc4 = 1;
+    this.glob.perc5 = 1;
+    this.glob.perc6 = 1;
+    this.glob.perc7 = 1;
+
+    this.glob.color1 = "NULL";
+    this.glob.color2 = "NULL";
+    this.glob.color3 = "NULL";
+    this.glob.color4 = "NULL";
+    this.glob.color5 = "NULL";
+    this.glob.color6 = "NULL";
+    this.glob.color7 = "NULL";
+
+    this.glob.temp_hot            = 75;
+    this.glob.temp_nice           = 30;
+    this.glob.temp_cold           = 20;
+    this.glob.temp_colderer       = 10;
+    this.glob.temp_coldererer     = 0;
+
+    this.bigD = new Array(37);
+},
+
+////////////////////////////////////////
+
+initApp: function()
+{
+    this.weatherInsideCallbackFunc = this.weatherInsideCallback.bind(this);
+},
+
+////////////////////////////////////////
+
+tempConvert: function (data)
+    {
+    // color brewer colors (derived from an 11 step diverging scale)
+    var color_hot           = "#d73027"; //"#d73027"
+    var color_warmer        = "#f46d43";
+    var color_warm          = "#fdae61";
+
+    var color_nice          = "#ffffbf";
+    var color_cool          = "#e0f3f8";
+    var color_cold          = "#abd9e9";
+
+    var color_colder        = "#74add1";
+    var color_colderer      = "#4575b4";
+    var color_coldererer    = "#000000"; //"#313695"
+
+    var color_unknown       = "#AAAAAA";
+
+
+    var color = color_unknown;
+    var colorb = color_unknown;
+    var perc = 1;
+
+    if (data < 0)
+        {
+        color = color_unknown;
+        colorb = color_unknown;
+        perc  = 1;
+        }
+    else if (data < this.glob.temp_coldererer)
+        {
+        color = color_coldererer;
+        colorb = color_coldererer;
+        perc  = 1;
+        }
+    else if (data < this.glob.temp_colderer)
+        {
+        color = color_colderer;
+        colorb = color_coldererer;
+        perc = (data - this.glob.temp_coldererer) / (this.glob.temp_colderer - this.glob.temp_coldererer);
+        }
+    else if (data < this.glob.temp_cold)
+        {
+        color = color_cold;
+        colorb = color_colderer;
+        perc = (data - this.glob.temp_colderer) / (this.glob.temp_cold - this.glob.temp_colderer);
+        }
+    else if (data < this.glob.temp_nice)
+        {
+        color = color_nice;
+        colorb = color_cold;
+        perc = (data - this.glob.temp_cold) / (this.glob.temp_nice - this.glob.temp_cold);      
+        }       
+    else
+        {
+        color = color_warmer; //color_hot
+        colorb = color_nice;
+        perc = (data - this.glob.temp_nice) / (this.glob.temp_hot - this.glob.temp_nice);
+        }
+
+    if (perc > 1.0)
+        perc = 1.0;
+    if (perc < 0.0)
+        perc = 0.0;
+                
+    return [color, colorb, perc];
+},
+
+////////////////////////////////////////
+
+drawBox: function (boxLocX, boxLocY, boxHeight, boxWidth, colorOut, percOut)
+{
+    var b = null;
+
+    if (this.gwin.sampleSVG !== null)
+        {
+        b = this.gwin.sampleSVG.append("svg:rect")
+        .style("stroke", "grey")
+        .style("stroke-width", 1)
+        .style("fill", colorOut)
+        .style("fill-opacity", percOut)
+        .attr("x", boxLocX)
+        .attr("y", boxLocY)
+        .attr("rx", this.gwin.rounded)
+        .attr("ry", this.gwin.rounded)
+        .attr("height", boxHeight)
+        .attr("width", boxWidth);
+        }   
+},
+
+
+drawBorderlessBox: function (boxLocX, boxLocY, boxHeight, boxWidth, colorOut, percOut)
+{
+    if (this.gwin.sampleSVG !== null)
+     this.gwin.sampleSVG.append("svg:rect")
+        .style("stroke", colorOut)
+        .style("fill", colorOut)
+        .style("fill-opacity", percOut)
+        .attr("x", boxLocX)
+        .attr("y", boxLocY)
+        .attr("rx", this.gwin.rounded)
+        .attr("ry", this.gwin.rounded)
+        .attr("height", boxHeight)
+        .attr("width", boxWidth);
+},
+
+////////////////////////////////////////
+
+drawT: function (textLocX, textLocY, theText, textFontSize, justification)
+{
+    if (this.gwin.sampleSVG !== null)
+        this.gwin.sampleSVG.append("svg:text")
+            .attr("x", textLocX)
+            .attr("y", textLocY)
+            .style("fill", "#000")
+            .style("font-size", textFontSize)
+            .style("font-family", this.gwin.displayFont)
+            .style("text-anchor", justification)
+            .text(theText);   
+},
+
+drawTWhite: function (textLocX, textLocY, theText, textFontSize, justification)
+{
+    if (this.gwin.sampleSVG !== null)
+        this.gwin.sampleSVG.append("svg:text")
+            .attr("x", textLocX)
+            .attr("y", textLocY)
+            .style("fill", "#FFF")
+            .style("font-size", textFontSize)
+            .style("font-family", this.gwin.displayFont)
+            .style("text-anchor", justification)
+            .text(theText);   
+},
+
+drawText: function (textLocX, textLocY, theText, textFontSize)
+{
+    this.drawT(textLocX, textLocY, theText, textFontSize, "middle");
+},
+
+drawTextLeft: function (textLocX, textLocY, theText, textFontSize)
+{
+    this.drawT(textLocX, textLocY, theText, textFontSize, "start");  
+},
+
+drawTextRight: function (textLocX, textLocY, theText, textFontSize)
+{
+    this.drawT(textLocX, textLocY, theText, textFontSize, "end");  
+},
+
+drawTextRightWhite: function (textLocX, textLocY, theText, textFontSize)
+{
+    this.drawTWhite(textLocX, textLocY, theText, textFontSize, "end");  
+},
+
+
+////////////////////////////////////////
+
+drawBasicStuff: function ()
+{
+    this.drawTWhite(50, 50, "CAVE2 Monitor", "40px");
+
+    var today = new Date();
+    var currentTime = (today.getMonth()+1) + "/" + today.getDate()  + "/"
+    + today.getFullYear() + " - " + today.getHours() + ":" + today.getMinutes()
+
+    this.drawTextRightWhite(1200, 50, currentTime, "40px");
+
+    this.drawTWhite(50, 390, "GPU Memory", this.gwin.smallFontSize);
+    this.drawTWhite(50, 420, "CPU Cores", this.gwin.smallFontSize);
+    this.drawTWhite(50, 550, "RAM", this.gwin.smallFontSize);
+    this.drawTWhite(50, 570, "Network Out", this.gwin.smallFontSize);
+    this.drawTWhite(50, 590, "Network In", this.gwin.smallFontSize);
+},
+
+
+////////////////////////////////////////
+
+weatherInsideCallback: function(error, datasetTextIn)
+{
+    if (error)
+        {
+        console.log("weatherInsideCallback - error");
+        return;
+        }
+
+    var parsedCSV = d3.csv.parseRows(datasetTextIn);
+    var d, d2;
+
+    // should be 37 lines in parsedCSV
+    //console.log(parsedCSV.length);
+    //console.log(parsedCSV[0][0]); // entire line 1 (master)
+    //console.log(parsedCSV[15][0]); // entire line 2 (node 1)
+
+    this.gwin.roughDate = parsedCSV[0][0];
+
+    d = parsedCSV[0][0].slice(0, 21);
+    d2 = parsedCSV[0][0].slice(23, 38);
+
+    for (var c = 0; c < 37; c++)
+        this.bigD[c] = new Array(20); // 16 + 4
+
+    for (j=0; j<37; j+= 1)
+        {
+        for( i=0; i<16; i+= 1)
+            {
+            this.bigD[j][i] = + parsedCSV[j][0].slice(41+(4*i), 44+(4*i));
+            }
+
+        this.bigD[j][16] = + parsedCSV[j][0].slice(105, 110);
+        this.bigD[j][17] = + parsedCSV[j][0].slice(113, 118);
+        this.bigD[j][18] = + parsedCSV[j][0].slice(119, 125);
+        this.bigD[j][19] = Math.round(100 / 64 * (+ parsedCSV[j][0].slice(126, 132)));
+        //console.log("bigD["+j+"] is " + this.bigD[j]);
+        }
+  
+    this.drawAll();
+},
+
+updateInsideTemp: function ()
+{ 
+     d3.text("http://lyra.evl.uic.edu:9000/html/cluster.txt" + '?' +
+         Math.floor(Math.random() * 10000000), this.weatherInsideCallbackFunc);
+},
+
+drawOneNode: function (x, y, proc, p, row)
+{
+    var i, j, num, t, topX;
+
+    if (p === 2) // processors - value ranges from 0 to 16
+        {
+        for (i=0; i<8; i++)
+            for (j=0; j<2; j++)
+            {
+                num = i + j*8;
+                topX = x+50+j*20;
+                topY = y+ i*16;
+                btmY = y- i*16;
+
+                if (row < 1)
+                    theY = topY
+                else
+                    theY = btmY;
+
+                if (num < Math.floor(proc))
+                    {
+                    c = this.tempConvert(100); // full 
+                    }
+                else if (num < (Math.ceil(proc)) )
+                    {
+                    t = (proc - Math.floor(proc))*100;
+                    c = this.tempConvert(t); // partial
+                    }
+                else
+                    {
+                    c = this.tempConvert(0); // off
+                    }
+
+                this.glob.colorOut = c[0];
+                this.glob.colorOutb = c[1];
+                this.glob.percOut = c[2];
+
+                this.drawBox(topX, theY, 14, 20, this.glob.colorOut, this.glob.percOut);
+                this.drawBox(topX, theY, 14, 20, this.glob.colorOutb, 1.0 - this.glob.percOut);
+            }
+
+        //this.drawTextRightWhite(x+80, y+12, proc.toFixed(1), "10px"); // 0 - 1600
+        }
+    else if ((p == 1) || (p == 3)) // GPU and memory space percentage
+        {
+        c = this.tempConvert(proc);
+        this.glob.colorOut = c[0];
+        this.glob.colorOutb = c[1];
+        this.glob.percOut = c[2];
+
+        this.drawBox(x+50, y, 14, 40, this.glob.colorOut, this.glob.percOut);
+        this.drawBox(x+50, y, 14, 40, this.glob.colorOutb, 1.0 - this.glob.percOut);
+
+        if (proc < this.glob.temp_colderer)
+            this.drawTextRightWhite(x+85, y+12, proc.toFixed(0)+"%", this.gwin.smallFontSize);
+        else
+            this.drawTextRight(x+85, y+12, proc.toFixed(0)+"%", this.gwin.smallFontSize);
+        }
+    else // networking values
+        {
+        c = this.tempConvert(proc);
+        this.glob.colorOut = c[0];
+        this.glob.colorOutb = c[1];
+        this.glob.percOut = c[2];
+
+        this.drawBox(x+50, y, 14, 40, this.glob.colorOut, this.glob.percOut);
+        this.drawBox(x+50, y, 14, 40, this.glob.colorOutb, 1.0 - this.glob.percOut);
+
+        //if (proc < this.glob.temp_colderer)
+            //this.drawTextRightWhite(x+80, y+12, proc.toFixed(0), "10px");
+        //else
+            //this.drawTextRight(x+80, y+12, proc.toFixed(0), "10px");
+        }
+},
+
+
+////////////////////////////////////////
+
+drawInsideTemp: function ()
+{
+    var procTotal = 0;
+
+
+    for (j=0; j<37; j+= 1)
+    {
+        procTotal = 0;
+        //console.log("bigD["+j+"] is " + this.bigD[j]);
+        for( i=0; i<16; i+= 1)
+            procTotal += Number(this.bigD[j][i]);
+
+        //total processor usage 0-16
+
+        if (j === 0)    // 0
+            {
+            this.drawTWhite((j+2)*30-8, 330, "master", this.gwin.smallFontSize);
+            this.drawOneNode((j)*30, 300, this.bigD[j][16], 1, -1); // GPU %age
+            this.drawOneNode((j)*30, 160, procTotal / 100,  2, -1); // CPU
+            this.drawOneNode((j)*30, 140, this.bigD[j][19], 3, -1); // memory %age
+            this.drawOneNode((j)*30, 120, this.bigD[j][18], 4, -1); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j)*30, 100, this.bigD[j][17], 5, -1); // net in < 1000, 1000 - 100000, > 100000
+            }
+
+        else if ( (j % 2) === 1) // 1 3 5 7
+            {
+            this.drawTWhite((j+4)*30+5, 330, j, this.gwin.smallFontSize);
+            this.drawOneNode((j+2)*30, 300, this.bigD[j][16], 1, 0); // GPU %age
+            this.drawOneNode((j+2)*30, 160, procTotal / 100,  2, 0); // CPU
+            this.drawOneNode((j+2)*30, 140, this.bigD[j][19], 3, 0); // memory %age
+            this.drawOneNode((j+2)*30, 120, this.bigD[j][18], 4, 0); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+2)*30, 100, this.bigD[j][17], 5, 0); // net in < 1000, 1000 - 100000, > 100000
+            }
+
+        else if ( (j % 2) === 0) // 2 4 6 8
+            {
+            this.drawTWhite((j+3)*30+5, 370, j, this.gwin.smallFontSize);
+            this.drawOneNode((j+1)*30, 380, this.bigD[j][16], 1, 1); // GPU %age
+            this.drawOneNode((j+1)*30, 520, procTotal / 100,  2, 1); // CPU
+            this.drawOneNode((j+1)*30, 540, this.bigD[j][19], 3, 1); // memory %age
+            this.drawOneNode((j+1)*30, 560, this.bigD[j][18], 4, 1); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+1)*30, 580, this.bigD[j][17] ,5, 1); // net in < 1000, 1000 - 100000, > 100000
+            }            
+        }
+},
+
+////////////////////////////////////////
+
+updateAll: function ()
+{
+    console.log("updating data");
+    this.updateInsideTemp(); 
+},
+
+////////////////////////////////////////
+
+drawAll: function ()
+{
+    var regionX, regionY;
+
+    // clear out the existing drawing
+    if (this.gwin.sampleSVG !== null)
+        this.gwin.sampleSVG.selectAll("*").remove();
+
+    // draw the background
+    this.drawBorderlessBox(0, 0, this.gwin.canvasHeight, this.gwin.canvasWidth, this.gwin.canvasBackground, 1);
+
+    // draw the foreground elements
+    this.drawBasicStuff();
+    this.drawInsideTemp(); 
+},
+
+////////////////////////////////////////
+
+updateWindow: function (){
+
+    var x = d3.select(this.myTag).style("width") ;
+    var y = d3.select(this.myTag).style("height") ;
+
+   this.gwin.sampleSVG.attr("width", x)
+        .attr("height", y)
+        .attr("viewBox", "0 0 " + this.gwin.canvasWidth + " " + this.gwin.canvasHeight)
+        .attr("preserveAspectRatio", "xMinYMin meet");
+},
+
+////////////////////////////////////////
+
+    init: function(id, width, height, resrc, date) {
+        // call super-class 'init'
+        arguments.callee.superClass.init.call(this, id, "div", width, height, resrc, date);
+
+        this.maxFPS = 0.1;
+
+        // Get width height from the supporting div     
+        var divWidth  = this.element.clientWidth;
+        var divHeight = this.element.clientHeight;
+
+        this.element.id = "div" + id;
+
+        // backup of the context
+        var self = this;
+
+        // attach the SVG into the this.element node provided to us
+        var box="0,0,"+this.gwin.canvasWidth+","+this.gwin.canvasHeight;
+        this.svg = d3.select(this.element).append("svg:svg")
+            .attr("width",   divWidth)
+            .attr("height",  divHeight)
+            .attr("viewBox", box)
+            .attr("preserveAspectRatio", "xMinYMin meet"); // new
+        this.gwin.sampleSVG = this.svg;
+
+        this.initApp();
+        this.updateAll();
+        //this.draw_d3(date);
+    },
+
+    load: function(state, date) {
+        this.refresh(date);
+    },
+
+    draw_d3: function(date) {
+
+        //this.drawBasicStuff();
+        this.updateWindow();
+    },
+    
+    draw: function(date) {
+        this.updateAll();
+    },
+
+    resize: function(date) {
+        this.svg.attr('width' ,  parseInt(this.element.clientWidth,10) +"px");
+        this.svg.attr('height' , parseInt(this.element.clientHeight,10)  +"px");
+
+        this.updateWindow();
+        this.refresh(date);
+    },
+
+    event: function(eventType, pos, user, data, date) {
+    //event: function(eventType, userId, x, y, data, date) {
+        if (eventType === "pointerPress" && (data.button === "left") ) {
+        }
+        if (eventType === "pointerMove" ) {
+        }
+        if (eventType === "pointerRelease" && (data.button === "left") ) {
+        }
+    }
+    
+});
