d7328ce20d5e3aca83852746ace43ee6af528da4
Andy J <ajohnson@uic.edu>
2014-08-30 19:35:04 -0500

    added net monitoring from original app
    Former-commit-id: 77ad044d35dc286684affd5f07e90401bb834e48

--- public_HTTPS/uploads/apps/cave2_monitor/cave2_monitor.js
+++ public_HTTPS/uploads/apps/cave2_monitor/cave2_monitor.js
@@ -78,13 +78,34 @@ var cave2_monitor = SAGE2_App.extend( {
     this.glob.temp_coldererer     = 0;
 
     this.bigD = new Array(37);
+
+    this.bigN1 = new Array(37);
+    this.bigN2 = new Array(37);
+
+    this.connection = 0;
+    this.net1Connection = 0;
+    this.net2Connection = 0;
+
+    this.today = new Date();
+    this.net1Time = new Date();
+    this.net2Time = new Date();
 },
 
 ////////////////////////////////////////
 
 initApp: function()
 {
+    var i;
+
     this.weatherInsideCallbackFunc = this.weatherInsideCallback.bind(this);
+    this.network1CallbackFunc = this.network1Callback.bind(this);
+    this.network2CallbackFunc = this.network2Callback.bind(this);
+
+    for (i=0; i <37; i++)
+        {
+        this.bigN1[0] = 1; // start assuming all connections are up
+        this.bigN2[0] = 1; // start assuming all connections are up
+    }
 },
 
 ////////////////////////////////////////
@@ -222,6 +243,19 @@ drawTWhite: function (textLocX, textLocY, theText, textFontSize, justification)
             .text(theText);   
 },
 
+drawTRed: function (textLocX, textLocY, theText, textFontSize, justification)
+{
+    if (this.gwin.sampleSVG !== null)
+        this.gwin.sampleSVG.append("svg:text")
+            .attr("x", textLocX)
+            .attr("y", textLocY)
+            .style("fill", "#F22")
+            .style("font-size", textFontSize)
+            .style("font-family", this.gwin.displayFont)
+            .style("text-anchor", justification)
+            .text(theText);   
+},
+
 drawText: function (textLocX, textLocY, theText, textFontSize)
 {
     this.drawT(textLocX, textLocY, theText, textFontSize, "middle");
@@ -247,21 +281,141 @@ drawTextRightWhite: function (textLocX, textLocY, theText, textFontSize)
 
 drawBasicStuff: function ()
 {
-    this.drawTWhite(50, 50, "CAVE2 Monitor", "40px");
+    this.drawTWhite(45, 50, "CAVE2 Monitor", "40px");
 
-    var today = new Date();
-    var currentTime = (today.getMonth()+1) + "/" + today.getDate()  + "/"
-    + today.getFullYear() + " - " + today.getHours() + ":" + ("0" + today.getMinutes()).slice (-2)
+    if ((this.connection === 0) || (this.net1Connection == 0) || (this.net2Connection == 0))
+        this.drawTRed(450, 50, "Connection Lost", "40px");
 
-    this.drawTextRightWhite(1200, 50, currentTime, "40px");
+    this.today = new Date();
+    var currentTime = (this.today.getMonth()+1) + "/" + this.today.getDate()  + "/"
+    + this.today.getFullYear() + " - " + this.today.getHours() + ":"
+    + ("0" + this.today.getMinutes()).slice (-2)
+    + ":" + ("0" + this.today.getSeconds()).slice (-2);
 
-    this.drawTWhite(50, 390, "GPU Memory", this.gwin.smallFontSize);
-    this.drawTWhite(50, 420, "CPU Cores", this.gwin.smallFontSize);
-    this.drawTWhite(50, 550, "RAM", this.gwin.smallFontSize);
-    this.drawTWhite(50, 570, "Network Out", this.gwin.smallFontSize);
-    this.drawTWhite(50, 590, "Network In", this.gwin.smallFontSize);
+    this.drawTextRightWhite(1205, 50, currentTime, "40px");
+
+    this.drawTWhite(40, 390, "GPU Memory",  "10px", "start");
+    this.drawTWhite(40, 420, "CPU Cores",   "10px", "start");
+    this.drawTWhite(40, 555, "CPU Memory",  "10px", "start");
+    this.drawTWhite(40, 580, "Network Out", "10px", "start");
+    this.drawTWhite(40, 600, "Network In",  "10px", "start");
 },
 
+
+////////////////////////////////////////
+
+network1Callback: function(error, datasetTextIn)
+{
+    var status, parsedNet1, line;
+
+    if (error)
+        {
+        console.log("network 1 Callback - error");
+        return;
+        }
+
+    // could also have a problem if length is less than 37
+    // need to deal with that
+
+    parsedNet1 = d3.csv.parseRows(datasetTextIn);
+    line = parsedNet1[0]; // eg Sat Aug 30 17:55:36 CDT 2014
+    status = line[0].split(" ");
+    time = status[3].split(":");
+
+    this.net1Time.setDate(status[2]);
+    this.net1Time.setFullYear(status[5]);
+    this.net1Time.setHours(time[0]);
+    this.net1Time.setMinutes(time[1]);
+    this.net1Time.setSeconds(time[2]);
+
+    var timeSinceLastConnect1 = Math.floor((this.today - this.net1Time)/1000);
+    if (timeSinceLastConnect1 > 30)
+        this.net1Connection = 0;
+    else
+        this.net1Connection = 1;
+
+    /*
+    setDate()   Sets the day of the month of a date object
+setFullYear()   Sets the year (four digits) of a date object
+setHours()  Sets the hour of a date object
+setMilliseconds()   Sets the milliseconds of a date object
+setMinutes()    Set the minutes of a date object
+setMonth()  Sets the month of a date object
+setSeconds()    Sets the seconds of a date object
+*/
+
+    
+    //console.log(parsedNet1);
+    // line 0 is date and time
+    // lines 1-36 are nodes
+    for (i=1; i <= 37; i++)
+        {
+        line = parsedNet1[i];
+        status = line[0].split(" ");
+        if (status[1] === "UP")
+            {
+                // update network status array location to be up
+                this.bigN1[i-1] = 1;
+            }
+        else
+            {
+                // update network status array location to be down
+                //console.log ("node " + i + " is down");
+                this.bigN1[i-1] = 0;
+            }
+        }
+},
+
+network2Callback: function(error, datasetTextIn)
+{
+    var status, parsedNet2, line;
+
+    if (error)
+        {
+        console.log("network 2 Callback - error");
+        return;
+        }
+
+    // could also have a problem if length is less than 37
+    // need to deal with that
+
+    parsedNet2 = d3.csv.parseRows(datasetTextIn);
+    line = parsedNet2[0]; // eg Sat Aug 30 17:55:36 CDT 2014
+    status = line[0].split(" ");
+    time = status[3].split(":");
+
+    this.net2Time.setDate(status[2]);
+    this.net2Time.setFullYear(status[5]);
+    this.net2Time.setHours(time[0]);
+    this.net2Time.setMinutes(time[1]);
+    this.net2Time.setSeconds(time[2]);
+
+    var timeSinceLastConnect2 = Math.floor((this.today - this.net2Time)/1000);
+    if (timeSinceLastConnect2 > 30)
+        this.net2Connection = 0;
+    else
+        this.net2Connection = 1;
+
+    //console.log(parsedNet1);
+    // line 0 is date and time
+    // lines 1-36 are nodes
+    for (i=1; i <= 37; i++)
+        {
+        line = parsedNet2[i];
+        status = line[0].split(" ");
+        if (status[1] === "UP")
+            {
+                // update network status array location to be up
+                this.bigN2[i-1] = 1; // for now
+            }
+        else
+            {
+                // update network status array location to be down
+                //console.log ("node " + i + " is down");
+                this.bigN2[i-1] = 0; 
+            }
+        }
+},
 
 ////////////////////////////////////////
 
@@ -269,40 +423,66 @@ weatherInsideCallback: function(error, datasetTextIn)
 {
     if (error)
         {
-        console.log("weatherInsideCallback - error");
+        console.log("lyra data Callback - error");
         return;
         }
 
     var parsedCSV = d3.csv.parseRows(datasetTextIn);
     var d, d2;
 
-    // should be 37 lines in parsedCSV
-    //console.log(parsedCSV.length);
-    //console.log(parsedCSV[0][0]); // entire line 1 (master)
-    //console.log(parsedCSV[15][0]); // entire line 2 (node 1)
-
-    this.gwin.roughDate = parsedCSV[0][0];
-
-    d = parsedCSV[0][0].slice(0, 21);
-    d2 = parsedCSV[0][0].slice(23, 38);
-
-    for (var c = 0; c < 37; c++)
-        this.bigD[c] = new Array(20); // 16 + 4
-
-    for (j=0; j<37; j+= 1)
+    if (parsedCSV.length >= 37)
         {
-        for( i=0; i<16; i+= 1)
+        this.connection = 1;
+
+        // should be 37 lines in parsedCSV
+        //console.log(parsedCSV.length);
+        //console.log(parsedCSV[0][0]); // entire line 1 (master)
+        //console.log(parsedCSV[27][0]); // entire line 2 (node 1)
+
+        this.gwin.roughDate = parsedCSV[0][0];
+
+        d = parsedCSV[0][0].slice(0, 21);
+        d2 = parsedCSV[0][0].slice(23, 38);
+
+        for (var c = 0; c < 37; c++)
+            this.bigD[c] = new Array(20); // 16 + 4
+
+        for (j=0; j<37; j+= 1)
             {
-            this.bigD[j][i] = + parsedCSV[j][0].slice(41+(4*i), 44+(4*i));
+            for( i=0; i<16; i+= 1)
+                {
+                this.bigD[j][i] = + parsedCSV[j][0].slice(41+(4*i), 44+(4*i));
+                }
+
+            this.bigD[j][16] = + parsedCSV[j][0].slice(105, 110);
+            this.bigD[j][17] = + parsedCSV[j][0].slice(113, 118);
+            this.bigD[j][18] = + parsedCSV[j][0].slice(119, 125);
+            this.bigD[j][19] = Math.round(100 / 64 * (+ parsedCSV[j][0].slice(126, 132)));
+            //console.log("bigD["+j+"] is " + this.bigD[j]);
             }
 
-        this.bigD[j][16] = + parsedCSV[j][0].slice(105, 110);
-        this.bigD[j][17] = + parsedCSV[j][0].slice(113, 118);
-        this.bigD[j][18] = + parsedCSV[j][0].slice(119, 125);
-        this.bigD[j][19] = Math.round(100 / 64 * (+ parsedCSV[j][0].slice(126, 132)));
-        //console.log("bigD["+j+"] is " + this.bigD[j]);
         }
-  
+    else // zero out all the display values
+        {
+            this.connection = 0;
+
+            for (var c = 0; c < 37; c++)
+                this.bigD[c] = new Array(20); // 16 + 4
+
+            for (j=0; j<37; j+= 1)
+                {
+                for( i=0; i<16; i+= 1)
+                    {
+                    this.bigD[j][i] = 0;
+                    }
+
+                this.bigD[j][16] = 0;
+                this.bigD[j][17] = 0;
+                this.bigD[j][18] = 0;
+                this.bigD[j][19] = 0;
+                }
+        }
+
     this.drawAll();
 },
 
@@ -310,6 +490,18 @@ updateInsideTemp: function ()
 { 
      d3.text("http://lyra.evl.uic.edu:9000/html/cluster.txt" + '?' +
          Math.floor(Math.random() * 10000000), this.weatherInsideCallbackFunc);
+},
+
+updateNetwork1: function ()
+{ 
+     d3.text("http://lyra.evl.uic.edu:9000/html/ping.txt" + '?' +
+         Math.floor(Math.random() * 10000000), this.network1CallbackFunc);
+},
+
+updateNetwork2: function ()
+{ 
+     d3.text("http://lyra.evl.uic.edu:9000/html/pingcavewave.txt" + '?' +
+         Math.floor(Math.random() * 10000000), this.network2CallbackFunc);
 },
 
 drawOneNode: function (x, y, proc, p, row)
@@ -324,8 +516,8 @@ drawOneNode: function (x, y, proc, p, row)
             {
                 num = i + j*8;
                 topX = x+50+j*20;
-                topY = y+ i*16;
-                btmY = y- i*16;
+                topY = y + i*16;
+                btmY = y - i*16;
 
                 if (row < 1)
                     theY = topY
@@ -366,15 +558,22 @@ drawOneNode: function (x, y, proc, p, row)
         this.drawBox(x+50, y, 14, 40, this.glob.colorOut, this.glob.percOut);
         this.drawBox(x+50, y, 14, 40, this.glob.colorOutb, 1.0 - this.glob.percOut);
 
-        if (proc < this.glob.temp_colderer)
-            this.drawTextRightWhite(x+85, y+12, proc.toFixed(0)+"%", this.gwin.smallFontSize);
+        if (proc <= 0)
+            this.drawTextRightWhite(x+85, y+11, "", "10px");
+        else if (proc < this.glob.temp_colderer)
+            this.drawTextRightWhite(x+85, y+11, proc.toFixed(0)+"%", "10px");
         else
-            this.drawTextRight(x+85, y+12, proc.toFixed(0)+"%", this.gwin.smallFontSize);
+            this.drawTextRight(x+85, y+11, proc.toFixed(0)+"%", "10px");
         }
     else // networking values
         // net out < 1000, 1000 - 100000, > 100000
         {
-            if (proc < 1000)
+            if (proc <= 0)
+                {
+                    modNet = 0;
+                    modNetText = "";
+                }
+            else if (proc < 1000)
                 {
                     modNet = 30 * proc / 1000;
                     modNetText = Math.floor(proc) + "K";
@@ -398,11 +597,11 @@ drawOneNode: function (x, y, proc, p, row)
         this.drawBox(x+50, y, 14, 40, this.glob.colorOut, this.glob.percOut);
         this.drawBox(x+50, y, 14, 40, this.glob.colorOutb, 1.0 - this.glob.percOut);
 
-        if (proc < 500)
-            this.drawTextRightWhite(x+85, y+12, modNetText, "10px");
+        if (proc < 600)
+            this.drawTextRightWhite(x+85, y+11, modNetText, "10px");
         else
-            this.drawTextRight(x+85, y+12, modNetText, "10px");
-        }    
+            this.drawTextRight(x+85, y+11, modNetText, "10px");
+        }
 },
 
 
@@ -411,6 +610,8 @@ drawOneNode: function (x, y, proc, p, row)
 drawInsideTemp: function ()
 {
     var procTotal = 0;
+    var netsUp = 0;
+    var netColor;
 
 
     for (j=0; j<37; j+= 1)
@@ -422,34 +623,49 @@ drawInsideTemp: function ()
 
         //total processor usage 0-16
 
+        netsUp = this.bigN1[j] + this.bigN2[j]; // should be 2 if all working well
+        if (netsUp === 1)
+            netColor = "yellow"
+        else if (netsUp === 0)
+            netColor = "red"
+
         if (j === 0)    // 0
             {
-            this.drawTWhite((j+2)*30-8, 330, "master", this.gwin.smallFontSize);
+            this.drawTWhite((j+2)*30+10, 335, "master", "18px", "middle");
             this.drawOneNode((j)*30, 300, this.bigD[j][16], 1, -1); // GPU %age
             this.drawOneNode((j)*30, 160, procTotal / 100,  2, -1); // CPU
-            this.drawOneNode((j)*30, 140, this.bigD[j][19], 3, -1); // memory %age
-            this.drawOneNode((j)*30, 120, this.bigD[j][18], 4, -1); // net out < 1000, 1000 - 100000, > 100000
-            this.drawOneNode((j)*30, 100, this.bigD[j][17], 5, -1); // net in < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j)*30, 135, this.bigD[j][19], 3, -1); // memory %age
+            this.drawOneNode((j)*30, 110, this.bigD[j][18], 4, -1); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j)*30,  90, this.bigD[j][17], 5, -1); // net in < 1000, 1000 - 100000, > 100000
+
+            if (netsUp < 2)
+                    this.drawBorderlessBox((j+1)*30+10, 80, 260, 60, netColor, 0.15);
             }
 
         else if ( (j % 2) === 1) // 1 3 5 7
             {
-            this.drawTWhite((j+4)*30+5, 330, j, this.gwin.smallFontSize);
+            this.drawTWhite((j+4)*30+10, 335, j, "18px", "middle");
             this.drawOneNode((j+2)*30, 300, this.bigD[j][16], 1, 0); // GPU %age
             this.drawOneNode((j+2)*30, 160, procTotal / 100,  2, 0); // CPU
-            this.drawOneNode((j+2)*30, 140, this.bigD[j][19], 3, 0); // memory %age
-            this.drawOneNode((j+2)*30, 120, this.bigD[j][18], 4, 0); // net out < 1000, 1000 - 100000, > 100000
-            this.drawOneNode((j+2)*30, 100, this.bigD[j][17], 5, 0); // net in < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+2)*30, 135, this.bigD[j][19], 3, 0); // memory %age
+            this.drawOneNode((j+2)*30, 110, this.bigD[j][18], 4, 0); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+2)*30,  90, this.bigD[j][17], 5, 0); // net in < 1000, 1000 - 100000, > 100000
+
+            if (netsUp < 2)
+                    this.drawBorderlessBox((j+3)*30+10, 80, 260, 60, netColor, 0.15);
             }
 
         else if ( (j % 2) === 0) // 2 4 6 8
             {
-            this.drawTWhite((j+3)*30+5, 370, j, this.gwin.smallFontSize);
+            this.drawTWhite((j+3)*30+10, 370, j, "18px", "middle");
             this.drawOneNode((j+1)*30, 380, this.bigD[j][16], 1, 1); // GPU %age
             this.drawOneNode((j+1)*30, 520, procTotal / 100,  2, 1); // CPU
-            this.drawOneNode((j+1)*30, 540, this.bigD[j][19], 3, 1); // memory %age
-            this.drawOneNode((j+1)*30, 560, this.bigD[j][18], 4, 1); // net out < 1000, 1000 - 100000, > 100000
-            this.drawOneNode((j+1)*30, 580, this.bigD[j][17] ,5, 1); // net in < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+1)*30, 545, this.bigD[j][19], 3, 1); // memory %age
+            this.drawOneNode((j+1)*30, 570, this.bigD[j][18], 4, 1); // net out < 1000, 1000 - 100000, > 100000
+            this.drawOneNode((j+1)*30, 590, this.bigD[j][17] ,5, 1); // net in < 1000, 1000 - 100000, > 100000
+
+            if (netsUp < 2)
+                    this.drawBorderlessBox((j+2)*30+10, 350, 260, 60, netColor, 0.15);
             }            
         }
 },
@@ -458,8 +674,9 @@ drawInsideTemp: function ()
 
 updateAll: function ()
 {
-    console.log("updating data");
     this.updateInsideTemp(); 
+    this.updateNetwork1();
+    this.updateNetwork2();
 },
 
 ////////////////////////////////////////
