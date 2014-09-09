11b9888fd71fb39ac818b49260f13922836e33f0
Todd Margolis <tmargo@gmail.com>
2014-09-08 18:06:35 -0700

    retrying commit & push
    Former-commit-id: 977b08255f074228bce1d22c17d665c82b73430e

--- public_HTTPS/uploads/apps/GoTime/GoTime.js
+++ public_HTTPS/uploads/apps/GoTime/GoTime.js
@@ -33,7 +33,6 @@ var GoTime = SAGE2_App.extend( {
 
 		// application specific 'init'
 		this.element.id = id;
-		console.log(id);
 
 		this.time = date;
 		this.dragging = false;
@@ -42,6 +41,8 @@ var GoTime = SAGE2_App.extend( {
 
 		// building up the state object
 		this.state.frame = 1;
+		this.state.numFrames = 20;
+		this.state.playing = false;
 
 		// may need a global handler for callbacks (i.e. scope pollution)
 		GoTimeList[id] = this;
@@ -50,13 +51,13 @@ var GoTime = SAGE2_App.extend( {
 		var my_div = null;
 		var newDiv = null;
 		function addElement () {
-			// create a new div element and give it some content
+			// create a new div element to hold the currentFrame
 			var newDiv = document.createElement("div");
 
 			var div = document.createElement('DIV');
 			newDiv.id = "currentFrame";
 			newDiv.style['position'] = 'absolute';
-			newDiv.style['top'] = '30px';
+			newDiv.style['top'] = '15px';
 			newDiv.style['width'] = 'auto';
 			newDiv.style['height'] = 'auto';
 			newDiv.style['float'] = 'left';
@@ -125,9 +126,10 @@ var GoTime = SAGE2_App.extend( {
 	updateFrame: function(ff) {
 		this.state.frame = ff.currentFrame;
 		this.state.numFrames = ff.numFrames;
-		console.log("frame", this.state.frame);
+		// console.log("frame", this.state.frame);
 		var f = document.getElementById('currentFrame');
-		f.innerHTML = this.state.frame;
+		if(f !== null)
+			f.innerHTML = this.state.frame;
 	},
 
 	load: function(state, date) {
@@ -135,54 +137,79 @@ var GoTime = SAGE2_App.extend( {
 			// this.state.frame   = state.frame;
 
 			var _this = this;
-			_this.numFrames = 25;
 
 			// UI stuff
 			_this.controls.addButton({type:"rewind", action:function(appObj, date){
 				appObj.state.frame = 1;
 				appObj.setLabelText();
 				appObj.refresh(date);
-				appObj.wsio.emit('changeFrame', {frame: appObj.state.frame});
+				appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
 			}});
 			_this.controls.addButton({type:"prev", action:function(appObj, date){
 				if(appObj.state.frame <= 1) return;
 				appObj.state.frame--;
 				appObj.setLabelText();
 				appObj.refresh(date);
-				appObj.wsio.emit('changeFrame', {frame: appObj.state.frame});
+				appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
 			}});
 	
-			_this.controls.addSlider({begin:1,end:_this.numFrames,increments:1,appObj:_this, property:"state.frame", action:function(appObj, date){
+			_this.controls.addSlider({begin:1,end:_this.state.numFrames,increments:1,appObj:_this, property:"state.frame", action:function(appObj, date){
 				appObj.setLabelText();
 				appObj.refresh(date);
-				appObj.wsio.emit('changeFrame', {frame: appObj.state.frame});
+				appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
 			}});
-			var labelWidth = ("" + _this.numFrames).length * 2 + 3; // 3 for the spaces and the '/'
+			var labelWidth = ("" + _this.state.numFrames).length * 2 + 3; // 3 for the spaces and the '/'
 
 			_this.controls.addLabel({textLength:labelWidth,appObj:_this, property:"frameValText"});
 
 			_this.controls.addButton({type:"next", action:function(appObj, date){
-				if (appObj.state.frame  >= appObj.numFrames) return;
+				if (appObj.state.frame  >= appObj.state.numFrames) return;
 				appObj.state.frame++;
 				appObj.setLabelText();
 				appObj.refresh(date);
-				appObj.wsio.emit('changeFrame', {frame: appObj.state.frame});
+				appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
 			}});
 			_this.controls.addButton({type:"fastforward", action:function(appObj, date){
-				appObj.state.frame = appObj.numFrames;
+				appObj.state.frame = appObj.state.numFrames;
 				appObj.setLabelText();
 				appObj.refresh(date);
-				appObj.wsio.emit('changeFrame', {frame: appObj.state.frame});
+				appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
 			}});
 
-			_this.controls.addButton({type:"add", action:function(appObj, date){
-				console.log("Add Keyframe");
-				// appObj.wsio.emit('addKeyframe', {frame: appObj.state.frame});
+			_this.controls.addButton({type:"play-pause", action:function(appObj, date){
+				console.log("Play");
+				var fps = 5;
+				appObj.state.playing = !appObj.state.playing;
+				if(appObj.state.playing){
+					if(appObj.state.frame == appObj.state.numFrames)
+						appObj.state.frame = 0;
+					appObj.state.frame++;
+					appObj.setLabelText();
+					appObj.refresh(date);
+					appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
+					appObj.state.GoTimePlayer = setInterval(function(){
+						if(appObj.state.frame < appObj.state.numFrames){
+							appObj.state.frame++;
+							appObj.setLabelText();
+							appObj.refresh(date);
+							appObj.wsio.emit('changeFrame', {currentFrame: appObj.state.frame, numFrames: appObj.state.numFrames});
+						}else{
+							clearInterval(appObj.state.GoTimePlayer);
+						}
+					}, 1000/fps);
+				}else{
+					clearInterval(appObj.state.GoTimePlayer);
+				}
 			}});
-			_this.controls.addButton({type:"sub", action:function(appObj, date){
-				console.log("Remove Keyframe");
-				// appObj.wsio.emit('removeKeyframe', {frame: appObj.state.frame});
-			}});
+
+			// _this.controls.addButton({type:"add", action:function(appObj, date){
+			// 	console.log("Add Keyframe");
+			// 	// appObj.wsio.emit('addKeyframe', {currentFrame: appObj.state.frame});
+			// }});
+			// _this.controls.addButton({type:"sub", action:function(appObj, date){
+			// 	console.log("Remove Keyframe");
+			// 	// appObj.wsio.emit('removeKeyframe', {currentFrame: appObj.state.frame});
+			// }});
 
 			
 			_this.setLabelText();
@@ -280,7 +307,7 @@ var GoTime = SAGE2_App.extend( {
 				this.refresh(date);
 			}
 			if(data.code === 39 && data.state === "up"){ // Right Arrow
-				if(this.state.frame >= this.numFrames) return;
+				if(this.state.frame >= this.state.numFrames) return;
 				this.state.frame++;
 				this.setLabelText();
 				this.refresh(date);
@@ -288,12 +315,17 @@ var GoTime = SAGE2_App.extend( {
 		}
 
 
-		console.log("frame: " + this.state.frame);
+		console.log("currentFrame: " + this.state.frame);
 		this.refresh(date);
 	},
 
 	setLabelText: function(){
-		this.frameValText = this.state.frame + ' / ' + this.numFrames;
+		this.frameValText = this.state.frame + ' / ' + this.state.numFrames;
+	},
+
+	quit: function(){
+		console.log("quiting GoTime");
+		delete this;
 	}
 
 });
