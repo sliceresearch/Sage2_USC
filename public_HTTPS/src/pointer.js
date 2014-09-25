5aa2795690f94a855c70f089e24403bccfc3185e
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2014-09-25 17:22:03 -0500

    pointer mode switch toggles icons as well as color
    Former-commit-id: 7d2f3147c9f80a16679764710d38df1329f3d9be

--- public_HTTPS/src/pointer.js
+++ public_HTTPS/src/pointer.js
@@ -9,15 +9,18 @@
 // Copyright (c) 2014
 
 function pointer() {
-	this.div         = null;
-	this.snap        = null;
-	this.pointerIcon = null;
-	this.appModeIcon = null;
-	this.winModeIcon = null;
-	this.labelBG     = null;
-	this.labelText   = null;
-	this.color       = null;
-	this.mode        = null;
+	this.div               = null;
+	this.snap              = null;
+	this.pointerIcon       = null;
+	this.pointerIconLoaded = null;
+	this.appModeIcon       = null;
+	this.appModeIconLoaded = null;
+	this.winModeIcon       = null;
+	this.winModeIconLoaded = null;
+	this.labelBG           = null;
+	this.labelText         = null;
+	this.color             = null;
+	this.mode              = null;
 	
 	this.init = function(id, label, color, width, height) {
 		this.div = document.getElementById(id);
@@ -30,6 +33,17 @@ function pointer() {
 		var pointerIconSize = height * 0.7;
 		var pointerIconX = height * 0.2;
 		var pointerIconY  = height * 0.2;
+		this.pointerIconLoaded = false;
+		
+		var winModeIconSize = height * 0.275;
+		var winModeIconX = height * 0.375;
+		var winModeIconY = height * 0.72;
+		this.winModeIconLoaded = false;
+		
+		var appModeIconSize = height * 0.275;
+		var appModeIconX = height * 0.085;
+		var appModeIconY = height * 0.085;
+		this.appModeIconLoaded = false;
 		
 		var labelBGX = height * 0.675;
 		var labelBGY = height * 0.73;
@@ -52,21 +66,59 @@ function pointer() {
 			});
 			
 			_this.snap.append(_this.pointerIcon);
-			
-			_this.labelBG = _this.snap.rect(labelBGX, labelBGY, labelBGWidth, labelBGHeight, labelBGHeight/2, labelBGHeight/2).attr({
-				fill: "rgba(0, 0, 0, 0.4)"
+    		
+    		_this.pointerIconLoaded = true;
+			if(_this.winModeIconLoaded === true && _this.appModeIconLoaded)
+				_this.updateIconColors();
+		});
+		
+		Snap.load("images/SAGE2 Window Manipulation.svg", function(f) {
+			_this.winModeIcon = f.select("svg");
+			_this.winModeIcon.attr({
+				id: "winModeIcon",
+				x: winModeIconX,
+				y: winModeIconY,
+				width: winModeIconSize,
+				height: winModeIconSize,
+				preserveAspectRatio: "xMinYMin meet"
 			});
 			
-			_this.labelText = _this.snap.text(labelTextX, labelTextY, label).attr({
-				fill: "#FFFFFF",
-    			fontSize: labelTextSize + "px",
-    			fontFamily: "Verdana"
-    		});
-    		
-    		_this.labelBG.attr({width: _this.labelText.node.clientWidth + labelBGHeight});
-    		
-    		_this.updateIconColors();
+			_this.snap.append(_this.winModeIcon);
+			
+			_this.winModeIconLoaded = true;
+			if(_this.pointerIconLoaded === true && _this.appModeIconLoaded === true)
+				_this.updateIconColors();
 		});
+		
+		Snap.load("images/SAGE2 Application Interaction.svg", function(f) {
+			_this.appModeIcon = f.select("svg");
+			_this.appModeIcon.attr({
+				id: "appModeIcon",
+				x: appModeIconX,
+				y: appModeIconY,
+				width: appModeIconSize,
+				height: appModeIconSize,
+				preserveAspectRatio: "xMinYMin meet"
+			});
+			
+			_this.snap.prepend(_this.appModeIcon);
+			
+			_this.appModeIconLoaded = true;
+			if(_this.pointerIconLoaded === true && _this.winModeIconLoaded === true)
+				_this.updateIconColors();
+		});
+		
+		this.labelBG = this.snap.rect(labelBGX, labelBGY, labelBGWidth, labelBGHeight, labelBGHeight/2, labelBGHeight/2).attr({
+			fill: "rgba(0, 0, 0, 0.6)"
+		});
+		
+		this.labelText = this.snap.text(labelTextX, labelTextY, label).attr({
+			fill: "#FFFFFF",
+			fontSize: labelTextSize + "px",
+			fontFamily: "Verdana"
+		});
+		
+		this.labelBG.attr({width: this.labelText.node.clientWidth + labelBGHeight});
 	};
 	
 	this.setColor = function(color) {
@@ -88,31 +140,36 @@ function pointer() {
 	this.updateIconColors = function() {
 		 // window manipulation
 	    if(this.mode === 0) {
-	    	var rects = this.pointerIcon.select("rect");
-			if(rects) paths.attr({fill: this.color, stroke: "#000000"});
-			var circles = this.pointerIcon.select("circle");
-			if(circles) paths.attr({fill: this.color, stroke: "#000000"});
-			var ellipses = this.pointerIcon.select("ellipse");
-			if(ellipses) paths.attr({fill: this.color, stroke: "#000000"});
-			var polygons = this.pointerIcon.select("polygon");
-			if(polygons) polygons.attr({fill: this.color, stroke: "#000000"});
-			var paths = this.pointerIcon.select("path");
-			if(paths) paths.attr({fill: this.color, stroke: "#000000"});
+	    	this.colorSVG(this.pointerIcon, "#000000", this.color);
+	    	this.colorSVG(this.winModeIcon, "#000000", this.color);
+	    	this.colorSVG(this.appModeIcon, "#000000", this.color);
+	    	
+	    	this.winModeIcon.attr({display: ""});
+	    	this.appModeIcon.attr({display: "none"});
 	    }
 	    // application interaction
 	    else if(this.mode === 1) {
-	    	var rects = this.pointerIcon.select("rect");
-			if(rects) paths.attr({fill: "#000000", stroke: this.color});
-			var circles = this.pointerIcon.select("circle");
-			if(circles) paths.attr({fill: "#000000", stroke: this.color});
-			var ellipses = this.pointerIcon.select("ellipse");
-			if(ellipses) paths.attr({fill: "#000000", stroke: this.color});
-			var polygons = this.pointerIcon.select("polygon");
-			if(polygons) polygons.attr({fill: "#000000", stroke: this.color});
-			var paths = this.pointerIcon.select("path");
-			if(paths) paths.attr({fill: "#000000", stroke: this.color});
+	    	this.colorSVG(this.pointerIcon, this.color, "#000000");
+	    	this.colorSVG(this.winModeIcon, this.color, "#000000");
+	    	this.colorSVG(this.appModeIcon, this.color, "#000000");
+	    	
+	    	this.winModeIcon.attr({display: "none"});
+	    	this.appModeIcon.attr({display: ""});
 	    }
-	}
+	};
+	
+	this.colorSVG = function(svg, stroke, fill) {
+		var rects = svg.selectAll("rect");
+		if(rects) rects.attr({fill: fill, stroke: stroke});
+		var circles = svg.selectAll("circle");
+		if(circles) circles.attr({fill: fill, stroke: stroke});
+		var ellipses = svg.selectAll("ellipse");
+		if(ellipses) ellipses.attr({fill: fill, stroke: stroke});
+		var polygons = svg.selectAll("polygon");
+		if(polygons) polygons.attr({fill: fill, stroke: stroke});
+		var paths = svg.selectAll("path");
+		if(paths) paths.attr({fill: fill, stroke: stroke});
+	};
 }
 
 /*
