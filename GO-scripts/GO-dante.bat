77461e28966dbacc5129ff881241c7040f24a330
tmarrinan <tmarri2@uic.edu>
2014-10-16 11:48:09 -0500

    updated Dante Windows config
    Former-commit-id: 179dd5ce1482e8e6992572b1325a297335d90881

--- GO-scripts/GO-dante.bat
+++ GO-scripts/GO-dante.bat
@@ -1,3 +1,5 @@
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --allow-running-insecure-content --disable-session-crashed-bubble --window-size=4098,2304 --window-position=-8,-1536 --app=https://localhost/audioManager.html /B
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-session-crashed-bubble --window-size=1366,768 --window-position=0,-1536 --app=https://localhost/audioManager.html /B
 timeout 1
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-session-crashed-bubble --allow-running-insecure-content --window-size=4098,2304 --window-position=-8,-1536 --app=https://localhost/?clientID=0 /B
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-session-crashed-bubble --window-size=4098,2304 --window-position=0,-1536 --app=https://localhost/?clientID=0 /B
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-session-crashed-bubble --window-size=4098,2304 --window-position=0,-768 --app=https://localhost/?clientID=1 /B
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-session-crashed-bubble --window-size=4098,2304 --window-position=0,0 --app=https://localhost/?clientID=2 /B
