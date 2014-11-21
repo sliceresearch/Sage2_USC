3d1bb0f395010a83f30a1f2f3bc4c284a9bf870f
Andy J <ajohnson@uic.edu>
2014-11-20 18:58:32 -0600

    automatically makke one browser go fullscreen
    Former-commit-id: 6bd67cdf01d576f8aae0c828abe65aa96f7d9ade

--- GO-scripts/GO-mac-deimos.sh
+++ GO-scripts/GO-mac-deimos.sh
@@ -16,16 +16,20 @@ global_param="--args --new-window  --disable-popup-blocking --nfirst-run --use-g
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $global_param --window-size=640,480 --window-position=0,0 --app=https://localhost:1442/audioManager.html  &
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $global_param --window-size=640,480 --window-position=0,0 --app=https://localhost:1443/audioManager.html  &
 
-sleep 1
+sleep 5
 
 UDD=$HOME/.config/chrome-nfs/mac0
 mkdir -p $UDD/Default
 param="$global_param --window-position=0,0 --window-size=1080,1920 --user-data-dir=$UDD --app=https://localhost:1442/display.html?clientID="
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $param &
 
-sleep 1
+sleep 5
 
 UDD=$HOME/.config/chrome-nfs/mac1
 mkdir -p $UDD/Default
-param="$global_param --window-position=800,1200 --window-size=1920,1080 --user-data-dir=$UDD --app=https://localhost:1443/display.html?clientID=0"
+param="$global_param --window-position=1080,840 --window-size=1920,1080 --user-data-dir=$UDD --app=https://localhost:1443/display.html?clientID=0"
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $param &
+
+sleep 5
+
+osascript -e "tell application \"Google Chrome\"" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
