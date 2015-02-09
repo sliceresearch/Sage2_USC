7baa9aecbfd52e42938b2a53f754828f5fc5d91b
Victor Mateevitsi <mvictoras@gmail.com>
2015-02-09 16:35:47 -0600

    Fixed jshint timeout. Added public/src directory
    Former-commit-id: 8eb4e4a164c79d513926894fe57ec2be720d7d05

--- test/test-server.js
+++ test/test-server.js
@@ -3,7 +3,9 @@
 var assert = require('assert');
 
 describe('Running server.js', function() {
+    this.timeout(15000);
     it('Running sage2...', function(done) {
+        setTimeout(15000);
         var sage2 = require('../server');
         done();
     });
