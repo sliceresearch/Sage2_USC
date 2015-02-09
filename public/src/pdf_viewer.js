26d123dd8a13d8ea24e9ebd73a3c767b173ef737
krishnaBharadwaj <kbhara5@uic.edu>
2015-02-09 15:40:07 -0600

    Caption added to pdf slider
    Former-commit-id: a98a417d3288c0985218b8f2f5f4cc146a5a94b6

--- public/src/pdf_viewer.js
+++ public/src/pdf_viewer.js
@@ -218,7 +218,7 @@ function addWidgetControlsToPdfViewer (_this){
 		this.refresh(date);
 	}.bind(_this)});
 
-	_this.controls.addSlider({begin:1,end:_this.pdfDoc.numPages,increments:1,appHandle:_this, property:"state.page", action:function(date){
+	_this.controls.addSlider({begin:1,end:_this.pdfDoc.numPages,increments:1,appHandle:_this, property:"state.page", caption: "Page" action:function(date){
 		this.refresh(date);
 	}.bind(_this)});
 	_this.controls.finishedAddingControls();
