49e8a5ecb4cfc7f7c2c5563b805469b5084c4e5c
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2015-02-18 19:17:13 -0600

    cleaned install_dependencies.js
    Former-commit-id: 880d5045b0f569e08b2506eda8b6140ed0ecb711

--- install_dependencies.js
+++ install_dependencies.js
@@ -113,36 +113,6 @@ function rmdirSync(directory) {
 	fs.rmdirSync(directory);
 }
 
-
-/*
-
-	fs.mkdirSync("node_modules");
-if(!fileExistsSync(path.join("node_modules", platform)))
-	fs.mkdirSync(path.join("node_modules", platform));
-if(!fileExistsSync(path.join("node_modules", platform, "node_modules")))
-	fs.mkdirSync(path.join("node_modules", platform, "node_modules"));
-
-copyFileSync("package.json", path.join("node_modules", platform, "package.json"));
-
-var cmd = spawn('npm', ['install', '--skip-installed'], {cwd: path.join(__dirname, "node_modules", platform)});
-cmd.stdout.on('data', function (data) {
-	console.log("> " + data);
-});
-cmd.stderr.on('data', function (data) {
-	console.log("> " + data);
-});
-cmd.on('close', function (code) {
-	console.log("INSTALL FINISHED: " + code);
-});
-
-
-*/
-
-function copyFileSync(infilename, outfilename) {
-	var content = fs.readFileSync(infilename, {encoding: null} );
-	fs.writeFileSync(outfilename, content, {encoding: null});
-}
-
 function fileExistsSync(filename) {
 	if (_NODE_VERSION === 10 || _NODE_VERSION === 11) {
 		return fs.existsSync(filename);
