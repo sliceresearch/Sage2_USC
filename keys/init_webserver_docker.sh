727d77fd126cd577db7d09dd6e1d651e11149117
Victor Mateevitsi <mvictoras@gmail.com>
2015-02-24 16:47:04 -0600

    Docker fixes
    Former-commit-id: 8ff334ac3230d71e90a15fd06279bbdd15a426c2

--- keys/init_webserver_docker.sh
+++ keys/init_webserver_docker.sh
@@ -1,38 +1,31 @@
 #!/bin/sh
 
-# clean up
-# /bin/rm -f *.crt *.csr *.key *.org
-
-cd /sage2/keys
-
 # phony password
 password=foobar
-#server=$HOSTNAME
-#server=iridium.evl.optiputer.net
 server=$1
 
 echo "Start: CA"
-openssl genrsa -des3 -out ca.key  -passout pass:$password 1024
-openssl req -new -key ca.key -out ca.csr -passin pass:$password -subj "/CN=$server"
-openssl x509 -req -days 365 -in ca.csr -out $server-ca.crt -signkey ca.key  -passin pass:$password
+openssl genrsa -des3 -out /sage2/keys/ca.key  -passout pass:$password 1024
+openssl req -new -key /sage2/keys/ca.key -out /sage2/keys/ca.csr -passin pass:$password -subj "/CN=$server"
+openssl x509 -req -days 365 -in /sage2/keys/ca.csr -out /sage2/keys/$server-ca.crt -signkey /sage2/keys/ca.key  -passin pass:$password
 echo ""
 echo ""
 
 #FQDN - hostname (webserver)
 echo "Start Server Certificate"
-openssl genrsa -des3 -out $server-server.key -passout pass:$password 1024
-openssl req -new -key $server-server.key -out server.csr -passin pass:$password -subj "/CN=$server"
+openssl genrsa -des3 -out /sage2/keys/$server-server.key -passout pass:$password 1024
+openssl req -new -key /sage2/keys/$server-server.key -out /sage2/keys/server.csr -passin pass:$password -subj "/CN=$server"
 echo ""
 echo ""
 
 echo "Copy Server Certificate"
-cp $server-server.key server.key.org
-openssl rsa -in server.key.org -out $server-server.key -passin pass:$password
+cp /sage2/keys/$server-server.key /sage2/keys/server.key.org
+openssl rsa -in /sage2/keys/server.key.org -out /sage2/keys/$server-server.key -passin pass:$password
 echo ""
 echo ""
 
 echo "Sign Server Certificate"
-openssl x509 -req -days 365 -in server.csr -signkey $server-server.key -out $server-server.crt
+openssl x509 -req -days 365 -in /sage2/keys/server.csr -signkey /sage2/keys/$server-server.key -out /sage2/keys/$server-server.crt
 echo ""
 echo ""
 
@@ -42,12 +35,12 @@ certutil -d sql:/sage2/.pki/nssdb -L
 # delete the previous server key
 certutil -d sql:/sage2/.pki/nssdb -D -n $server
 # add the new key
-certutil -d sql:/sage2/.pki/nssdb -A -t "P,," -n $server -i $server-server.crt
+certutil -d sql:/sage2/.pki/nssdb -A -t "P,," -n $server -i /sage2/keys/$server-server.crt
 # print the DB again
 certutil -d sql:/sage2/.pki/nssdb -L
 echo ""
 echo "Finished"
 
-/bin/rm -f server.key.org server.csr ca.csr ca.key
+/bin/rm -f /sage2/keys/server.key.org /sage2/keys/server.csr /sage2/keys/ca.csr /sage2/keys/ca.key
 
 
