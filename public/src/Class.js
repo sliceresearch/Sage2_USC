6480d813a4671ccfdcf619f7a6a7adc292524b88
Luc Renambot <renambot@uic.edu>
2015-03-05 11:55:06 -0600

    Make Class.js strict and add documentation fields
    Former-commit-id: 2aabd386cfc4c787d7245abff378e7da7eabcd63

--- public/src/Class.js
+++ public/src/Class.js
@@ -8,27 +8,46 @@
 //
 // Copyright (c) 2014
 
+"use strict";
+
+/**
+ * Base class for Javascript
+ *
+ * @class Class
+ * @constructor
+ */
 function Class() { }
 
+/**
+ * Adds a constructor function to the class
+ *
+ * @method construct
+ */
 Class.prototype.construct = function() {};
 
+/**
+ * Method to extent a class, it adds a superClass and inherits the base prototype
+ *
+ * @method extend
+ * @return {Object} class definition
+ */
 Class.extend = function(def) {
 	var classDef = function() {
 		if (arguments[0] !== Class) { this.construct.apply(this, arguments); }
 	};
- 
+
 	var proto = new this(Class);
 	var superClass = this.prototype;
- 
+
 	for (var n in def) {
-		var item = def[n];                      
+		var item = def[n];
 		if (item instanceof Function) item.superClass = superClass;
 		proto[n] = item;
 	}
- 
+
 	classDef.prototype = proto;
- 
-	//Give this new class the same static extend method    
-	classDef.extend = this.extend;      
+
+	// Give this new class the same static extend method
+	classDef.extend = this.extend;
 	return classDef;
 };
