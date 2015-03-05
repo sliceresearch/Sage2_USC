77564a07149ea775940a1dc9d43b24d4470e109b
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2015-03-05 16:46:29 -0600

    fixed bug in new install script
    Former-commit-id: de3ddefb4e9df8fc5d19787e3dbbc905695fbeb8

--- install_dependencies.js
+++ install_dependencies.js
@@ -77,12 +77,17 @@ function install() {
 }
 
 function unzipModules() {
-	var key;
-	for(key in downloaded) {
-		unpacked[key+suffix] = false;
+	if(isEmpty(downloaded)) {
+		install();
 	}
-	for(key in unpacked) {
-		unzipModule(key);
+	else {
+		var key;
+		for(key in downloaded) {
+			unpacked[key+suffix] = false;
+		}
+		for(key in unpacked) {
+			unzipModule(key);
+		}
 	}
 }
 
@@ -139,6 +144,25 @@ function allTrueDict(dict) {
 		if(dict[key] !== true) return false;
 	}
 	return true;
+}
+
+function isEmpty(obj) {
+	// null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
 }
 
 /*
