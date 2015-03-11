ddca5230d33e5fc6e1cf4723160da20cddd5f329
Luc Renambot <renambot@uic.edu>
2015-03-10 19:30:53 -0500

    Doc for node-websocket.io.js
    Former-commit-id: f46d689824b0180a990ddcd81bf4b349d303e405

--- src/node-websocket.io.js
+++ src/node-websocket.io.js
@@ -9,32 +9,48 @@
 // Copyright (c) 2014
 
 /**
- @module WebSocket
+ * Lightweight object around websocket, handles string and binary communication
+ *
+ * @module WebSocket
+ * @requires ws
  */
 
 // require variables to be declared
 "use strict";
 
-
 var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 
+/**
+ * Client socket object
+ *
+ * @class WebsocketIO
+ * @constructor
+ * @param ws {Object} ULR of the server or actual websocket
+ * @param strictSSL {Bool} require or not SSL verification with a certiifcate
+ * @param openCallback {Function} callback when the socket opens
+ */
 function WebsocketIO(ws, strictSSL, openCallback) {
-	if(typeof ws === "string") this.ws = new WebSocket(ws, null, {rejectUnauthorized: strictSSL});
-	else this.ws = ws;
+	if (typeof ws === "string")
+		this.ws = new WebSocket(ws, null, {rejectUnauthorized: strictSSL});
+	else
+		this.ws = ws;
 
 	var _this = this;
 	this.messages = {};
-	if (this.ws.readyState === 1) this.remoteAddress = {address: this.ws._socket.remoteAddress, port: this.ws._socket.remotePort};
+	if (this.ws.readyState === 1)
+		this.remoteAddress = {address: this.ws._socket.remoteAddress, port: this.ws._socket.remotePort};
 
 	this.ws.on('error', function(err) {
 		if (err.errno === "ECONNREFUSED") return; // do nothing
 	});
+
 	this.ws.on('open', function() {
 		_this.ws.binaryType = "arraybuffer";
 		_this.remoteAddress = {address: _this.ws._socket.remoteAddress, port: _this.ws._socket.remotePort};
 		if(openCallback !== null) openCallback();
 	});
+
 	this.ws.on('message', function(message) {
 		if(typeof message === "string"){
 			var msg = JSON.parse(message);
@@ -57,6 +73,12 @@ function WebsocketIO(ws, strictSSL, openCallback) {
 	});
 }
 
+/**
+* Setting a callback when the socket closes
+*
+* @method onclose
+* @param callback {Function} function to execute after closing
+*/
 WebsocketIO.prototype.onclose = function(callback) {
 	var _this = this;
 	this.ws.on('close', function(){
@@ -64,22 +86,36 @@ WebsocketIO.prototype.onclose = function(callback) {
 	});
 };
 
+/**
+* Set a message handler for a given name
+*
+* @method on
+* @param name {String} name for the handler
+* @param callback {Function} handler to be called for a given name
+*/
 WebsocketIO.prototype.on = function(name, callback) {
 	this.messages[name] = callback;
 };
 
+/**
+* Send a message with a given name and payload (format> f:name d:payload)
+*
+* @method emit
+* @param name {String} name of the message (i.e. RPC)
+* @param data {Object} data to be sent with the message
+*/
 WebsocketIO.prototype.emit = function(name, data) {
 	var message;
 
-	if(name === null || name === ""){
+	if (name === null || name === "") {
 		console.log("Error: no message name specified");
 		return;
 	}
 
 	// send binary data as array buffer
-	if(Buffer.isBuffer(data)){
+	if (Buffer.isBuffer(data)) {
 		var funcName = Buffer.concat([new Buffer(name), new Buffer([0])]);
-		message = Buffer.concat([funcName, data]);
+		message      = Buffer.concat([funcName, data]);
 
 		try {
 			this.ws.send(message, {binary: true, mask: false}, function(err){
@@ -109,17 +145,37 @@ WebsocketIO.prototype.emit = function(name, data) {
 	}
 };
 
-// No JSON stringigy and no check version
+/**
+* Faster version for emit: No JSON stringigy and no check version
+*
+* @method emitString
+* @param data {Object} data to be sent as the message
+*/
 WebsocketIO.prototype.emitString = function(data) {
 	this.ws.send(data, {binary: false, mask: false});
 };
 
 
+/**
+ * Server socket object
+ *
+ * @class WebsocketIOServer
+ * @constructor
+ * @param data {Object} object containing .server or .port information
+ */
 function WebsocketIOServer(data) {
-	if(data.server !== undefined)    this.wss = new WebSocketServer({server: data.server, perMessageDeflate: false});
-	else if(data.port !== undefined) this.wss = new WebSocketServer({port: data.port, perMessageDeflate: false});
+	if (data.server !== undefined)
+		this.wss = new WebSocketServer({server: data.server, perMessageDeflate: false});
+	else if(data.port !== undefined)
+		this.wss = new WebSocketServer({port: data.port, perMessageDeflate: false});
 }
 
+/**
+* Setting a callback when a connection happens
+*
+* @method onconnection
+* @param callback {Function} function taking the new client (WebsocketIO) as parameter
+*/
 WebsocketIOServer.prototype.onconnection = function(callback) {
 	this.wss.on('connection', function(ws) {
 		ws.binaryType = "arraybuffer";
