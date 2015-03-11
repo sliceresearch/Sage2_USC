cb1258d23257030de692ed3e4b4e3ac4c2d54b12
Luc Renambot <renambot@uic.edu>
2015-03-10 20:14:55 -0500

    Doc template and style editing in  src/node-radialmenu.js
    Former-commit-id: 209aa267da8d63e96467d74910209df095dd2570

--- src/node-radialmenu.js
+++ src/node-radialmenu.js
@@ -9,120 +9,158 @@
 // Copyright (c) 2014
 
 /**
- @module radialmenu
+ * Radial menu for a given pointer
+ *
+ * @module radialmenu
  */
 
 // require variables to be declared
 "use strict";
 
 // unused: var radialMenuCenter = { x: 210, y: 210 }; // scale applied in ctor
-var radialMenuDefaultSize = { x: 425, y: 425 }; // scale applied in ctor
+var radialMenuDefaultSize      = { x: 425, y: 425 }; // scale applied in ctor
 var thumbnailWindowDefaultSize = { x: 1224, y: 860 };
 
+/**
+ * Class RadialMenu
+ *
+ * @class RadialMenu
+ * @constructor
+ */
 function RadialMenu(id, ptrID, ui) {
-	this.id = id;
+	this.id        = id;
 	this.pointerid = ptrID;
-	this.label = "";
-	this.color = [255, 255, 255];
-	this.left = 0;
-	this.top = 0;
-	this.visible = true;
+	this.label     = "";
+	this.color     = [255, 255, 255];
+	this.left      = 0;
+	this.top       = 0;
+	this.visible   = true;
+	this.wsio      = undefined;
 	this.thumbnailWindowOpen = false;
-	this.wsio = undefined;
 
 	// Default
-	this.radialMenuScale = ui.widgetControlSize * 0.03;
-	this.radialMenuSize = { x: radialMenuDefaultSize.x * this.radialMenuScale, y: radialMenuDefaultSize.y * this.radialMenuScale };
+	this.radialMenuScale     = ui.widgetControlSize * 0.03;
+	this.radialMenuSize      = { x: radialMenuDefaultSize.x * this.radialMenuScale, y: radialMenuDefaultSize.y * this.radialMenuScale };
 	this.thumbnailWindowSize = { x: thumbnailWindowDefaultSize.x * this.radialMenuScale, y: thumbnailWindowDefaultSize.y * this.radialMenuScale };
-
-	this.activeEventIDs = [];
+	this.activeEventIDs      = [];
 }
 
+/**
+*
+*
+* @method getInfo
+*/
 RadialMenu.prototype.getInfo = function() {
-	return { id: this.pointerid, x: this.left, y: this.top, radialMenuSize: this.radialMenuSize, thumbnailWindowSize: this.thumbnailWindowSize, radialMenuScale: this.radialMenuScale, visble: this.visible };
+	return {id: this.pointerid, x: this.left, y: this.top, radialMenuSize: this.radialMenuSize, thumbnailWindowSize: this.thumbnailWindowSize, radialMenuScale: this.radialMenuScale, visble: this.visible };
 };
 
+/**
+*
+*
+* @method start
+*/
 RadialMenu.prototype.start = function() {
 	this.visible = true;
 };
 
+/**
+*
+*
+* @method stop
+*/
 RadialMenu.prototype.stop = function() {
 	this.visible = false;
 };
 
+/**
+*
+*
+* @method openThumbnailWindow
+*/
 RadialMenu.prototype.openThumbnailWindow = function(data) {
 	this.thumbnailWindowOpen = data.thumbnailWindowOpen;
 };
 
+/**
+*
+*
+* @method setPosition
+*/
 RadialMenu.prototype.setPosition = function(data) {
 	this.left = data.x;
-	this.top = data.y;
+	this.top  = data.y;
 };
 
+/**
+*
+*
+* @method hasEventID
+*/
 RadialMenu.prototype.hasEventID = function(id) {
-	if( this.activeEventIDs.indexOf(id) === -1 )
+	if (this.activeEventIDs.indexOf(id) === -1)
 		return false;
 	else
 		return true;
 };
 
+/**
+*
+*
+* @method isEventOnMenu
+*/
 RadialMenu.prototype.isEventOnMenu = function(data) {
-	if( this.visible === true)
-	{
+	if (this.visible === true) {
 		// If over radial menu bounding box
-		if( (data.x > this.left - this.radialMenuSize.x/2) && (data.x < this.left - this.radialMenuSize.x/2 + this.radialMenuSize.x) &&
-			(data.y > this.top  - this.radialMenuSize.y/2) && (data.y < this.top - this.radialMenuSize.y/2  + this.radialMenuSize.y) )
-		{
+		if ((data.x > this.left - this.radialMenuSize.x/2) && (data.x < this.left - this.radialMenuSize.x/2 + this.radialMenuSize.x) &&
+			(data.y > this.top  - this.radialMenuSize.y/2) && (data.y < this.top - this.radialMenuSize.y/2  + this.radialMenuSize.y) ) {
 			return true;
 		}
 		// Else if over thumbnail window bounding box
-		else if((data.x > this.left + this.radialMenuSize.x/2) && (data.x < this.left + this.radialMenuSize.x/2 + this.thumbnailWindowSize.x) &&
-				(data.y > this.top - this.radialMenuSize.y/2)  && (data.y < this.top - this.radialMenuSize.y/2  + this.thumbnailWindowSize.y) )
-		{
-			if( this.thumbnailWindowOpen === true )
-			{
+		else if ((data.x > this.left + this.radialMenuSize.x/2) && (data.x < this.left + this.radialMenuSize.x/2 + this.thumbnailWindowSize.x) &&
+				(data.y > this.top - this.radialMenuSize.y/2)  && (data.y < this.top - this.radialMenuSize.y/2  + this.thumbnailWindowSize.y) ) {
+			if (this.thumbnailWindowOpen === true) {
 				return true;
 			}
 		}
-
 	}
 	return false;
 };
 
+/**
+*
+*
+* @method onEvent
+*/
 RadialMenu.prototype.onEvent = function(data) {
 	var idIndex = this.activeEventIDs.indexOf(data.id);
-	if( idIndex !== -1 && data.type === "pointerRelease" )
-		this.activeEventIDs.splice( idIndex );
+	if (idIndex !== -1 && data.type === "pointerRelease")
+		this.activeEventIDs.splice(idIndex);
 
-	if( this.visible === true)
-	{
+	if (this.visible === true) {
 		// Press over radial menu, drag menu
 		//console.log((this.left - this.radialMenuSize.x/2), " < ", position.x, " < ", (this.left - this.radialMenuSize.x/2 + this.radialMenuSize.x) );
 		//console.log((this.top - this.radialMenuSize.y/2), " < ", position.y, " < ", (this.top - this.radialMenuSize.y/2 + this.radialMenuSize.y) );
 
 		// If over radial menu bounding box
-		if( (data.x > this.left - this.radialMenuSize.x/2) && (data.x < this.left - this.radialMenuSize.x/2 + this.radialMenuSize.x) &&
-			(data.y > this.top  - this.radialMenuSize.y/2) && (data.y < this.top - this.radialMenuSize.y/2  + this.radialMenuSize.y) )
-		{
+		if ((data.x > this.left - this.radialMenuSize.x/2) && (data.x < this.left - this.radialMenuSize.x/2 + this.radialMenuSize.x) &&
+			(data.y > this.top  - this.radialMenuSize.y/2) && (data.y < this.top - this.radialMenuSize.y/2  + this.radialMenuSize.y) ) {
 			//this.windowInteractionMode = false;
 
-			if( this.visible === true && data.type === "pointerPress" )
-				this.activeEventIDs.push( data.id );
+			if (this.visible === true && data.type === "pointerPress")
+				this.activeEventIDs.push(data.id);
 
 			return true;
 		}
 		// Else if over thumbnail window bounding box
-		else if( this.thumbnailWindowOpen === true && (data.x > this.left + this.radialMenuSize.x/2) && (data.x < this.left + this.radialMenuSize.x/2 + this.thumbnailWindowSize.x) &&
-				 (data.y > this.top - this.radialMenuSize.y/2)  && (data.y < this.top - this.radialMenuSize.y/2  + this.thumbnailWindowSize.y) )
-		{
+		else if (this.thumbnailWindowOpen === true && (data.x > this.left + this.radialMenuSize.x/2) && (data.x < this.left + this.radialMenuSize.x/2 + this.thumbnailWindowSize.x) &&
+				(data.y > this.top - this.radialMenuSize.y/2)  && (data.y < this.top - this.radialMenuSize.y/2  + this.thumbnailWindowSize.y) ) {
 			//this.windowInteractionMode = false;
 
-			if( this.visible === true && data.type === "pointerPress" )
-				this.activeEventIDs.push( data.id );
+			if (this.visible === true && data.type === "pointerPress")
+				this.activeEventIDs.push(data.id);
 			return true;
 		}
-		else if( this.activeEventIDs.indexOf(data.id) != -1 )
-		{
+		else if (this.activeEventIDs.indexOf(data.id) !== -1) {
 			return true;
 		}
 	}
