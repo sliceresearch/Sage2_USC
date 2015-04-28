614a51f3989b15b27f175dfba6baed32ad131b08
Luc Renambot <renambot@uic.edu>
2015-04-27 21:59:43 -0500

    Linting
    Former-commit-id: fc31ddeacafbea4774390c36a7aa14e522764ade
    Former-commit-id: f13fb56f13487938fcb3ec110ddcdfd3c59ad98b

--- public/src/SAGE2_Display.js
+++ public/src/SAGE2_Display.js
@@ -1016,23 +1016,21 @@ function setupListeners() {
 						//ctrl.animate({"path":path, "fill":fill}, delay, mina.bounce);
 					}
 				}
-				ctrlId = ctrl.parent().attr("id").replace("button","");
+				ctrlId = ctrl.parent().attr("id").replace("button", "");
 			}
-			else{
-				ctrlId = ctrl.parent().attr("id").replace("slider","");
+			else {
+				ctrlId = ctrl.parent().attr("id").replace("slider", "");
 				action = "sliderRelease";
 			}
-			
-			
-			
+
 			/*
 			var func = ctrl.parent().data("call");
 			if (func !== undefined && func !== null)
 				func(new Date());
 			*/
 			var appId = data.ctrl.appId;
-			var app = applications[appId];
-			switch(ctrlId){
+			var app   = applications[appId];
+			switch(ctrlId) {
 				case "CloseApp":
 					if (isMaster){
 						wsio.emit('closeAppFromControl', {appId:appId});
@@ -1047,12 +1045,12 @@ function setupListeners() {
 					app.event("widgetEvent", null, data.user, {ctrlId: ctrlId, action:action}, new Date());
 					break;
 			}
-			
-			//Check whether a request for clone was made.
-			if(app.cloneable === true && app.requestForClone === true){
+
+			// Check whether a request for clone was made.
+			if (app.cloneable === true && app.requestForClone === true) {
 				app.requestForClone = false;
 				console.log("cloning app:" + appId);
-				if(isMaster)
+				if (isMaster)
 					wsio.emit('createAppClone', {id : appId, cloneData: app.cloneData});
 			}
 
@@ -1065,7 +1063,7 @@ function setupListeners() {
 		var slider = ctrl.parent();
 		var appId = data.ctrl.appId;
 		var app = applications[appId];
-		var ctrlId = slider.attr("id").replace("slider","");
+		var ctrlId = slider.attr("id").replace("slider", "");
 		app.event("widgetEvent", null, data.user, {ctrlId: ctrlId, action:"sliderLock"}, new Date());
 		/*
 		var func   = slider.data("lockCall");
@@ -1102,9 +1100,9 @@ function setupListeners() {
 		var appObj = getProperty(applications[slider.data("appId")], slider.data("appProperty"));
 		appObj.handle[appObj.property] = updatedSliderInfo.sliderValue;
 
-		var appId = data.ctrl.appId;
-		var app = applications[appId];
-		var ctrlId = slider.attr("id").replace("slider","");
+		var appId  = data.ctrl.appId;
+		var app    = applications[appId];
+		var ctrlId = slider.attr("id").replace("slider", "");
 		app.event("widgetEvent", null, data.user, {ctrlId: ctrlId, action:"sliderUpdate"}, new Date());
 		/*
 		var func = slider.data("updateCall");
@@ -1122,7 +1120,7 @@ function setupListeners() {
 				insertTextIntoTextInputWidget(textInput, data.code, data.printable);
 			}
 			else{
-				var ctrlId = textInput.attr("id").replace("textInput","");
+				var ctrlId = textInput.attr("id").replace("textInput", "");
 				var blinkControlHandle = textInput.data("blinkControlHandle");
 				clearInterval(blinkControlHandle);
 				var app = applications[data.appId];
