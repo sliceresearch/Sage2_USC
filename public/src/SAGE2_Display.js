e9cae005b7770b52a34675915fe8da9ec2fe543f
Luc Renambot <renambot@uic.edu>
2015-05-10 00:26:54 -0500

    Use the png image for the window buttons (instead of webp)
    Former-commit-id: cd69554994fbd38f1364de23c43e4c65d1826503
    Former-commit-id: 37f6a5891bdea53845edefc9b4efdeb1b7eb7fac

--- public/src/SAGE2_Display.js
+++ public/src/SAGE2_Display.js
@@ -1082,7 +1082,8 @@ function createAppWindow(data, parentId, titleBarHeight, titleTextSize, offsetX,
 	if (ui.noDropShadow===true) windowTitle.style.boxShadow = "none";
 
 	var windowIcons = document.createElement("img");
-	windowIcons.src = "images/layout3.webp";
+	//windowIcons.src = "images/layout3.webp";
+	windowIcons.src = "images/layout3.png";
 	windowIcons.height = Math.round(titleBarHeight);
 	windowIcons.style.position = "absolute";
 	windowIcons.style.right    = "0px";
