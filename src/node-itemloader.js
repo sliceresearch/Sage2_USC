6e5a147c1f597214b8021172ea79d351180714dc
Tommy Marrinan <thomas.j.marrinan@gmail.com>
2015-05-28 17:12:40 -0500

    clean up of last commit
    Former-commit-id: cd7d34f943c9c35f29f683c6cbd1eebce16866b4
    Former-commit-id: b217a485676911df0d1a5778da07a183441741d5

--- src/node-itemloader.js
+++ src/node-itemloader.js
@@ -280,8 +280,6 @@ AppLoader.prototype.loadImageFromServer = function(width, height, mime_type, url
 	metadata.license     = "SAGE2-Software-License";
 	metadata.keywords    = ["image", "picture", "viewer"];
 
-	console.log(external_url);
-
 	var appInstance = {
 		id: null,
 		title: name,
