1607a5a6df3c5068fe8f9ceae47cb4095e441bf3
Tommy Marrinan <thomas.j.marrinan@gmail.com>
2015-05-29 14:00:58 -0500

    windows can be moved, fullscreened, and closed in app interaction mode by using title bar
    Former-commit-id: 0753be54d10620dc269403d8c0731894b67ef584
    Former-commit-id: f843fd04b290fc5868cbde481773187b5f7a48d5

--- src/node-interaction.js
+++ src/node-interaction.js
@@ -51,7 +51,7 @@ function Interaction(config) {
  */
 
 Interaction.prototype.selectMoveItem = function(moveItem, pointerX, pointerY) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
 	this.selectedMoveItem    = moveItem;
 	this.selectedMoveControl = null;
 	//this.selectedScrollItem  = null;
@@ -93,7 +93,7 @@ Interaction.prototype.releaseControl = function() {
  */
 
 Interaction.prototype.selectScrollItem = function(scrollItem) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
 
 	this.selectedMoveItem    = null;
 	this.selectedScrollItem  = scrollItem;
@@ -106,7 +106,7 @@ Interaction.prototype.selectScrollItem = function(scrollItem) {
  */
 
 Interaction.prototype.releaseItem = function(valid) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
 
 	var updatedItem = null;
 	if (!valid && this.selectedMoveItem !== null) {
@@ -137,7 +137,8 @@ Interaction.prototype.releaseItem = function(valid) {
  */
 
 Interaction.prototype.moveSelectedItem = function(pointerX, pointerY) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (this.selectedMoveItem === null) return null;
 
 	this.selectedMoveItem.left = pointerX + this.selectOffsetX;
@@ -232,7 +233,8 @@ Interaction.prototype.dropControl = function() {
  */
 
 Interaction.prototype.scrollSelectedItem = function(scale) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (this.selectedScrollItem === null) return null;
 
 	var iWidth = this.selectedScrollItem.width * scale;
@@ -279,7 +281,7 @@ Interaction.prototype.setHoverCornerItem = function(item) {
  */
 
 Interaction.prototype.selectResizeItem = function(resizeItem, pointerX, pointerY) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return;
 
 	this.selectedMoveItem    = null;
 	//this.selectedScrollItem  = null;
@@ -294,7 +296,8 @@ Interaction.prototype.selectResizeItem = function(resizeItem, pointerX, pointerY
  */
 
 Interaction.prototype.resizeSelectedItem = function(pointerX, pointerY) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (this.selectedResizeItem === null) return null;
 
 	var iWidth  = pointerX - this.selectedResizeItem.left + this.selectOffsetX;
@@ -348,7 +351,8 @@ Interaction.prototype.resizeSelectedItem = function(pointerX, pointerY) {
  */
 
 Interaction.prototype.maximizeSelectedItem = function(item) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (item === null) return null;
 
 	var wallRatio = this.configuration.totalWidth  / this.configuration.totalHeight;
@@ -392,7 +396,8 @@ Interaction.prototype.maximizeSelectedItem = function(item) {
 };
 
 Interaction.prototype.maximizeFullSelectedItem = function(item) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (item === null) return null;
 
 	// back up values for restore
@@ -425,7 +430,8 @@ Interaction.prototype.maximizeFullSelectedItem = function(item) {
  */
 
 Interaction.prototype.restoreSelectedItem = function(item) {
-	if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	//if (this.interactionMode !== MODE.WINDOW_MANAGEMENT) return null;
+	
 	if (item === null) return null;
 
 	item.left   = item.previous_left;
