c11a4d87daab81403c7b72df092e4b0911addbea
Luc Renambot <renambot@uic.edu>
2015-06-11 19:04:37 -0500

    Update key generation to 2048bits and SHA-256 to remove browser warnings
    Former-commit-id: 04c8851779d9537220f0106243338f1b9b7a2352
    Former-commit-id: 8d8bc4f04e3915cbc08e11758516bd68d1d2f344

--- keys/init_webserver.bat
+++ keys/init_webserver.bat
@@ -4,16 +4,16 @@ set password=foobar
 set server=%1
 
 rem echo "Start: CA"
-win32\openssl genrsa -des3 -out ca.key  -passout pass:%password% 1024
-win32\openssl req -new -key ca.key -out ca.csr -passin pass:%password% -subj "/CN=%server%"  -config win32\openssl.cnf
+win32\openssl genrsa -des3 -out ca.key  -passout pass:%password% 2048
+win32\openssl req -new -key ca.key -sha256 -out ca.csr -passin pass:%password% -subj "/CN=%server%"  -config win32\openssl.cnf
 win32\openssl x509 -req -days 365 -in ca.csr -out %server%-ca.crt -signkey ca.key  -passin pass:%password%
 rem echo ""
 rem echo ""
 
 rem FQDN - hostname (webserver)
 rem echo "Start Server Certificate"
-win32\openssl genrsa -des3 -out %server%-server.key -passout pass:%password% 1024
-win32\openssl req -new -key %server%-server.key -out server.csr -passin pass:%password% -subj "/CN=%server%" -config win32\openssl.cnf
+win32\openssl genrsa -des3 -out %server%-server.key -passout pass:%password% 2048
+win32\openssl req -new -sha256 -key %server%-server.key -out server.csr -passin pass:%password% -subj "/CN=%server%" -config win32\openssl.cnf
 rem echo ""
 rem echo ""
 
@@ -24,7 +24,7 @@ rem echo ""
 rem echo ""
 
 rem echo "Sign Server Certificate"
-win32\openssl x509 -req -days 365 -in server.csr -signkey %server%-server.key -out %server%-server.crt
+win32\openssl x509 -req -sha256 -days 365 -in server.csr -signkey %server%-server.key -out %server%-server.crt
 rem echo ""
 rem echo ""
 
