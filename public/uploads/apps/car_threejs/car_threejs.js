bf1040619c7e6c2e98b98de48be2a34f03ef9594
Luc Renambot <renambot@uic.edu>
2015-08-14 17:16:28 -0500

    Allow explicitely cross-origin texture for the 'car' demo
    Former-commit-id: 115aad8c5222b622dccb3ad8e1db27467b80b547
    Former-commit-id: 54c74b92c50bb80cbc2b93e970a07967dac52444

--- public/uploads/apps/car_threejs/car_threejs.js
+++ public/uploads/apps/car_threejs/car_threejs.js
@@ -91,6 +91,9 @@ var car_threejs = SAGE2_App.extend({
 		this.cameraCube = new THREE.PerspectiveCamera(25, this.width / this.width, 1, 10000);
 		this.sceneCube.add(this.cameraCube);
 
+		// Allow cross-origin access, needed for wall to wall sharing
+		THREE.ImageUtils.crossOrigin = 'anonymous';
+
 		var r    = this.resrcPath + "textures/";
 		var urls = [ r + "px.jpg", r + "nx.jpg", r + "py.jpg", r + "ny.jpg", r + "pz.jpg", r + "nz.jpg" ];
 		var textureCube = THREE.ImageUtils.loadTextureCube(urls);
@@ -135,7 +138,7 @@ var car_threejs = SAGE2_App.extend({
 
 		// Loader
 		var start = Date.now();
-		var loaderCTM = new THREE.CTMLoader(true);
+		var loaderCTM = new THREE.CTMLoader(false);
 
 		var position = new THREE.Vector3(-105, -78, -40);
 		var scale    = new THREE.Vector3(30, 30, 30);
