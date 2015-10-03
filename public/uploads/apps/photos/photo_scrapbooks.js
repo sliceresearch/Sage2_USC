9c8876df6e4fdb0e9b1773239cc0098438ffa737
Luc Renambot <renambot@uic.edu>
2015-10-03 09:27:31 -0500

    Updated 'photos' app to provide settings as a function instead of global variable. Should help when loading multiple instances of the app.
    Former-commit-id: 3c093cc65801ba0339e8f7fd945de991ad401ae9
    Former-commit-id: fc00d0bbc90f921a0addc3502d9e27ebe2f0ed46

--- public/uploads/apps/photos/photo_scrapbooks.js
+++ public/uploads/apps/photos/photo_scrapbooks.js
@@ -9,35 +9,41 @@
 // location gives the root directory of the images listed in list
 // name is what to name the button in the user interface for this album
 
-// as an example the chi album reads the webcam3.txt file which has the name of a 
+// as an example the chi album reads the webcam3.txt file which has the name of a
 //       single image to reload from the URL given in location: http://cdn.abclocal.go.com/three/wls/webcam/"
-//name
-//Loopscape.jpg
+// name
+// Loopscape.jpg
 //
 // whereas the photos.txt file for the sage album contains a list of images
-//name
-//addedsince2011/_LJL0075.jpg
-//addedsince2011/_LJL0388.jpg
-//addedsince2011/_LJL0408.jpg
+// name
+// addedsince2011/_LJL0075.jpg
+// addedsince2011/_LJL0388.jpg
+// addedsince2011/_LJL0408.jpg
 
 // the images can be stored in the same location as the text file with the list of images
 // or they can be stored in different URLs
 
+function photoAlbums() {
+	var SAGE2_photoAlbums = [];
+	SAGE2_photoAlbums[0] = {list: "https://lyra.evl.uic.edu:9000/sagewalls/photos.txt",
+		location: "https://lyra.evl.uic.edu:9000/sagewalls/",
+		name: "sage"};
+	SAGE2_photoAlbums[1] = {list: "https://lyra.evl.uic.edu:9000/webcam3.txt",
+		location: "http://cdn.abclocal.go.com/three/wls/webcam/",
+		name: "chi"};
 
-SAGE2_photoAlbums = [];
-SAGE2_photoAlbums[0] = {list:"https://lyra.evl.uic.edu:9000/sagewalls/photos.txt",
-        location:"https://lyra.evl.uic.edu:9000/sagewalls/",
-        name:"sage"};
-SAGE2_photoAlbums[1] = {list:"https://lyra.evl.uic.edu:9000/webcam3.txt",
-        location:"http://cdn.abclocal.go.com/three/wls/webcam/",
-        name:"chi"};
+	// load timer is how long to show a single image in seconds before loading
+	// the next one or refreshing the current one
+	var SAGE2_photoAlbumLoadTimer = 20;
 
-// load timer is how long to show a single image in seconds before loading 
-// the next one or refreshing the current one
-SAGE2_photoAlbumLoadTimer = 20;
+	// fade count is how many frames it takes to fade between the old and new image
+	var SAGE2_photoAlbumFadeCount = 20;
 
-// fade count is how many frames it takes to fade between the old and new image
-SAGE2_photoAlbumFadeCount = 20;
+	// canvas background gives the color of the background of the window
+	SAGE2_photoAlbumCanvasBackground = "black";
 
-// canvas background gives the color of the background of the window
-SAGE2_photoAlbumCanvasBackground = "black";
+	// return an object with all the settings
+	return {albums: SAGE2_photoAlbums, loadTimer: SAGE2_photoAlbumLoadTimer,
+		fadeCount: SAGE2_photoAlbumFadeCount, background: SAGE2_photoAlbumCanvasBackground};
+}
+
