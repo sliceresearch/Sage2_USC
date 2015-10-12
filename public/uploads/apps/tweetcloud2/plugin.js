a59458945fe0d8a343ec6b303b5c8e884581b29f
Luc Renambot <renambot@uic.edu>
2015-10-12 14:07:27 -0500

    Linting tweetcloud app
    Former-commit-id: 1566460eacfda6e43f650723843c30f58dced142
    Former-commit-id: 29bac3377df820966480acafe2103c6d746db4e1

--- public/uploads/apps/tweetcloud2/plugin.js
+++ public/uploads/apps/tweetcloud2/plugin.js
@@ -14,41 +14,46 @@ var sageutils = require('../../../../src/node-utils');            // provides th
 
 var twitter;
 
+var api = {
+	consumerKey: "fuyVLmYL4QMG8dhg4UDxiGYKK",
+	consumerSecret: "XPf8AxHF6l3VjNqJzWpIiy4t1B1nEun8kWbXdXHfMKhhNUK7ox",
+	accessToken: "213878544-eLXXES5ufR2XU2jI7SDAVnd4tgAwr5MqpE9S5gLZ",
+	accessSecret: "pjTo8R9ncGKVIqGOqX17nUAQNHLw2CWaWwP78MtT0Tv8x"
+};
+
 function processRequest(wsio, data, config) {
-		if (config.apis !== undefined &&
-				config.apis.twitter !== undefined &&
-				twitter === undefined) {
-			// twitter api
-			var Twit = require('twit');
-			console.log(sageutils.header('RPC') + 'building twitter object');
-			twitter = new Twit({
-				consumer_key:         config.apis.twitter.consumerKey,
-				consumer_secret:      config.apis.twitter.consumerSecret,
-				access_token:         config.apis.twitter.accessToken,
-				access_token_secret:  config.apis.twitter.accessSecret
-			});
-		}
-
-		if (twitter === null) {
-			if (data.broadcast === true) {
-				broadcast('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: null,
-					err: {message: "Twitter API not enabled in SAGE2 configuration"}}});
-			} else {
-				wsio.emit('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: null,
-					err: {message: "Twitter API not enabled in SAGE2 configuration"}}});
-			}
-			return;
-		}
-
-		console.log(sageutils.header('RPC') + 'searchTweets');
-
-		twitter.get('search/tweets', data.query, function(err, info, response) {
-			if (data.broadcast === true) {
-				broadcast('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: info, err: err}});
-			} else {
-				wsio.emit('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: info, err: err}});
-			}
+	if (twitter === undefined) {
+		// twitter api
+		var Twit = require('twit');
+		console.log(sageutils.header('RPC') + 'building twitter object');
+		twitter = new Twit({
+			consumer_key:         api.consumerKey,
+			consumer_secret:      api.consumerSecret,
+			access_token:         api.accessToken,
+			access_token_secret:  api.accessSecret
 		});
+	}
+
+	if (twitter === null) {
+		if (data.broadcast === true) {
+			broadcast('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: null,
+				err: {message: "Twitter API not enabled in SAGE2 configuration"}}});
+		} else {
+			wsio.emit('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: null,
+				err: {message: "Twitter API not enabled in SAGE2 configuration"}}});
+		}
+		return;
+	}
+
+	console.log(sageutils.header('RPC') + 'searchTweets');
+
+	twitter.get('search/tweets', data.query, function(err, info, response) {
+		if (data.broadcast === true) {
+			broadcast('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: info, err: err}});
+		} else {
+			wsio.emit('broadcast', {app: data.app, func: data.func, data: {query: data.query, result: info, err: err}});
+		}
+	});
 }
 
 module.exports = processRequest;
