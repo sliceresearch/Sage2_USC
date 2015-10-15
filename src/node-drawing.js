b4f648d612e6c9bb655f9fe11c9aea532508b1ad
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2015-10-15 16:58:53 -0500

    config file added
    Former-commit-id: dd51f7a6d84e9689f9b02ec1aac54288f0be0ca0
    Former-commit-id: 35db103a351a9c35a031f558d84129daaf2c7b4e

--- src/node-drawing.js
+++ src/node-drawing.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function DrawingManager(){
-	console.log("Create");
+	console.log("Created DrawingManager");
 }
 
-DrawingManager.prototype.pointerEvent(e,sourceID,posX,posY){
-	console.log("pointer event received: ")
+DrawingManager.prototype.pointerEvent= function(e,sourceID,posX,posY){
+	console.log("pointer event received");
 	console.log(e);
 	//e.type=5 --> pointer down
 	//e.type=4 --> pointer move
