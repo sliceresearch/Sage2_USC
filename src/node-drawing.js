974cc33b0cd8ecade31f96a539d9715da933f867
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2015-10-24 13:31:20 -0500

    linting
    Former-commit-id: e6c587b38582d2ca7780749e789e6404af173421
    Former-commit-id: 2dbd5a126cceba2536dc85df3707bf459e913a7f

--- src/node-drawing.js
+++ src/node-drawing.js
@@ -5,7 +5,7 @@ function DrawingManager(config) {
 	this.idPrequel = "drawing_";
 	this.clientIDandSockets = {};
 	this.newDrawingObject = {};
-	this.style = {fill: "none", stroke: "white", "stroke-width": "5px", "stroke-linecap"="round"};
+	this.style = {fill: "none", stroke: "white", "stroke-width": "5px", "stroke-linecap": "round"};
 	this.drawingMode = true;
 	this.drawState = [{id: "drawing_1",type: "path",options: { points: [{x: 100,y: 200}, {x: 200,y: 300}] }, style: this.style}];
 	this.drawingsUndone = [];
