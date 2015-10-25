1c736523a53e0d54fb510dc3e3ba002c0ffdf85f
Luc Renambot <renambot@uic.edu>
2015-10-25 14:07:53 -0500

    Added a test before handling the corner of an app.
    Former-commit-id: ddeea17113f17b5b6276b90fc7118611a5dd7d5e
    Former-commit-id: abe0a4282c4644a6ea98332a96d088b35ac4c45f

--- public/src/SAGE2_Display.js
+++ public/src/SAGE2_Display.js
@@ -536,13 +536,15 @@ function setupListeners() {
 
 	wsio.on('hoverOverItemCorner', function(elem_data) {
 		var selectedElem = document.getElementById(elem_data.elemId);
-		var dragCorner   = selectedElem.getElementsByClassName("dragCorner");
-		if (elem_data.flag) {
-			dragCorner[0].style.backgroundColor = "rgba(255,255,255,0.7)";
-			dragCorner[0].style.border = "2px solid #333333";
-		} else {
-			dragCorner[0].style.backgroundColor = "rgba(255,255,255,0.0)";
-			dragCorner[0].style.border = "none";
+		if (selectedElem) {
+			var dragCorner   = selectedElem.getElementsByClassName("dragCorner");
+			if (elem_data.flag) {
+				dragCorner[0].style.backgroundColor = "rgba(255,255,255,0.7)";
+				dragCorner[0].style.border = "2px solid #333333";
+			} else {
+				dragCorner[0].style.backgroundColor = "rgba(255,255,255,0.0)";
+				dragCorner[0].style.border = "none";
+			}
 		}
 	});
 
