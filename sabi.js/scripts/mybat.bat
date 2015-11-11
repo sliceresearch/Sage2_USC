49985fb04e4c74c774935805e13a32a58cfc2fa8
Luc Renambot <renambot@uic.edu>
2015-11-11 11:55:25 -0600

    Launch scripta
    Former-commit-id: 5949fa15286c9b0d6278a41b4e9d8c79bbb0e58a
    Former-commit-id: 1429a8e4bb656038c70c16446432d8ea994d32fb

--- sabi.js/scripts/mybat.bat
+++
@@ -1,2 +0,0 @@
-@rem off
-start iexplore.exe
---
+++ sabi.js/scripts/sage2_on.bat
@@ -0,0 +1,13 @@
+@rem off
+
+start /D ../.. sage2.bat
+
+timeout 2
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-popup-blocking --no-first-run --enable-accelerated-compositing --allow-file-access-from-files --disable-session-crashed-bubble --allow-running-insecure-content --window-size=800,300 --window-position=0,0 http://localhost/audioManager.html /B
+
+timeout 1
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --disable-popup-blocking --no-first-run --enable-accelerated-compositing --allow-file-access-from-files --disable-session-crashed-bubble --allow-running-insecure-content --window-size=800,450 --window-position=0,800 http://localhost/display.html?clientID=0 /B
+
+
