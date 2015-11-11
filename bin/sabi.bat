6938ff0c6aaa9ad99df07bcf9d029236c4190c66
Luc Renambot <renambot@uic.edu>
2015-11-11 13:24:05 -0600

    win
    Former-commit-id: 4d8ab1647cc414c12be8060e21d38f6d2cfaf04e
    Former-commit-id: 54b33a91e397bd6ca6050dcb352eb29dc54542bb

--- bin/sabi.bat
+++ bin/sabi.bat
@@ -2,4 +2,4 @@
 set PATH=%CD%\bin;%PATH%;
 start /D "%~dp0\sabi.js" node server.js -f config/sage2.json %*
 timeout 1
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window --no-first-run --window-size=400,800 --window-position=0,0 http://localhost:10000 /B
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"  --user-data-dir=chrome_sabi --new-window --no-first-run --window-size=400,800 --window-position=0,0 http://localhost:10000 /B
