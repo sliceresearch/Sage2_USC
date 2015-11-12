96bf42acaf3050cf2e9f46d05a57e331898781e3
Luc Renambot <renambot@uic.edu>
2015-11-11 18:52:44 -0600

    Added sage2_off script for Windows
    Former-commit-id: e8d5fe7ed8ec0e68a70d2bfa519b45f5996140b5
    Former-commit-id: f5d6221e23e0905c1a7fa6ba2aeb8163a61a167e

---
+++ sabi.js/scripts/sage2_off.bat
@@ -0,0 +1,8 @@
+@rem off
+
+rem close the server
+Taskkill /FI \"WindowTitle eq npm\" /F
+
+rem close chrome
+Taskkill /FI \"WindowTitle eq SAGE2: Display - Google Chrome\" /F
+Taskkill /FI \"WindowTitle eq SAGE2: Audio Manager - Google Chrome\" /F
