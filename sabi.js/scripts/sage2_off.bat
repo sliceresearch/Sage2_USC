d9fde83b4db90beba839b93243638b33ba71a463
Luc Renambot <renambot@uic.edu>
2015-11-11 22:48:19 -0600

    appveyor
    Former-commit-id: bc3f4aad07fb0d44490155ccba1f358113d7c19a
    Former-commit-id: fa7ff2a5a72572f6ed902c2716a54fa9ad919255

--- sabi.js/scripts/sage2_off.bat
+++ sabi.js/scripts/sage2_off.bat
@@ -1,8 +1,8 @@
 @rem off
 
 rem close the server
-Taskkill /FI \"WindowTitle eq npm\" /F
+Taskkill /FI "WindowTitle eq npm" /F
 
 rem close chrome
-Taskkill /FI \"WindowTitle eq SAGE2: Display - Google Chrome\" /F
-Taskkill /FI \"WindowTitle eq SAGE2: Audio Manager - Google Chrome\" /F
+Taskkill /FI "WindowTitle eq SAGE2: Display - Google Chrome" /F
+Taskkill /FI "WindowTitle eq SAGE2: Audio Manager - Google Chrome" /F
