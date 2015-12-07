4ed5112b5c5191ce25704432c4b0958f95a6325c
Luc Renambot <renambot@uic.edu>
2015-12-07 17:05:04 -0600

    Updating dependencies installation (for node v5 and npm v3)
    Former-commit-id: 84726392a0e83bd644dbb1e9e3fef57dbe3fde7a
    Former-commit-id: 3561e11d538c8f23a32cdf7d801b15fdda076063

--- install_dependencies.js
+++ install_dependencies.js
@@ -43,7 +43,7 @@ platformFull = platform === "win" ? "Windows" : platform === "mac" ? "Mac OS X" 
 // Target detection or force mode
 var target;
 var target_arg = process.argv.indexOf('--target');
-if (target_arg > 0 && process.argv.length > target_arg+1) target = process.argv[target_arg+1];
+if (target_arg > 0 && process.argv.length > target_arg + 1) target = process.argv[target_arg + 1];
 else target = process.versions.node;
 
 // Parsing node version numbers
@@ -63,23 +63,30 @@ if (nums[0] === 4 && nums[1] >=  0 && nums[2] >=  0) {
 	console.log("Node version " + process.versions.node + ". Using binaries for 4.0.0+.");
 	target = "4.0.0";
 }
+// Node v5.0.0 and above
+if (nums[0] === 5 && nums[1] >=  0 && nums[2] >=  0) {
+	console.log("Node version " + process.versions.node + ". Using binaries for 5.0.0+.");
+	target = "5.1.0";
+}
 
 console.log("Installing for " + platformFull + ", Node v" + target);
 
 var unpacked = [];
 
-if (fileExistsSync("node_modules")) rmdirSync("node_modules");
+if (fileExistsSync("node_modules")) {
+	rmdirSync("node_modules");
+}
 fs.mkdirSync("node_modules");
 
 
-var suffix = "_"+platform+"_"+target+".tar.gz";
+var suffix = "_" + platform + "_" + target + ".tar.gz";
 var packages = [
-	{name: "node-demux", url: "https://bitbucket.org/tmarrinan/binary-modules/downloads"},
+	{name: "node-demux",  url: "https://bitbucket.org/tmarrinan/binary-modules/downloads"},
 	{name: "websocketio", url: "https://bitbucket.org/tmarrinan/binary-modules/downloads"}
 ];
 
 var downloaded = {};
-for(var i=0; i<packages.length; i++){
+for (var i = 0; i < packages.length; i++) {
 	downloaded[packages[i].name] = false;
 }
 
@@ -89,24 +96,27 @@ packages.forEach(function(element, index, array) {
 	if (packageURL.protocol === "http:") isSecure = false;
 	else isSecure = true;
 
-	request({host: packageURL.host, path: packageURL.pathname+"/"+element.name+suffix}, isSecure, function(res) {
-		if(res.statusCode === 200) {
-			console.log("found binary package: " + element.name+suffix);
-			var writestream = fs.createWriteStream(path.join("node_modules", element.name+suffix));
+	request({host: packageURL.host, path: packageURL.pathname + "/" + element.name + suffix}, isSecure, function(res) {
+		if (res.statusCode === 200) {
+			console.log("found binary package: " + element.name + suffix);
+			var writestream = fs.createWriteStream(path.join("node_modules", element.name + suffix));
 			writestream.on('error', function(err) {
 				console.log(err);
 			});
 
-			res.on('end', function () {
+			res.on('end', function() {
 				downloaded[element.name] = true;
-				if(allTrueDict(downloaded)) unzipModules();
+				if (allTrueDict(downloaded)) {
+					unzipModules();
+				}
 			});
 			res.pipe(writestream);
-		}
-		else {
-			console.log("could not find binary package " + element.name+suffix + ". compiling instead.");
+		} else {
+			console.log("could not find binary package " + element.name + suffix + ". compiling instead.");
 			delete downloaded[element.name];
-			if(allTrueDict(downloaded)) unzipModules();
+			if (allTrueDict(downloaded)) {
+				unzipModules();
+			}
 		}
 	});
 });
@@ -126,10 +136,13 @@ function install() {
 		installCommand = "npm install --skip-installed --target=" + target + " --loglevel warn --production";
 
 	// Run the command
-	exec(installCommand, {encoding: "utf8", timeout: 0, maxBuffer: 1024*1024},
+	exec(installCommand, {encoding: "utf8", timeout: 0, maxBuffer: 1024 * 1024},
 		function(error, stdout, stderr) {
 			// fail or not
-			if (error) throw error;
+			if (error) {
+				console.log('Error', error, stderr);
+				throw error;
+			}
 			// wait for it...
 			clearInterval(timer);
 			process.stdout.write("\n");
@@ -140,13 +153,12 @@ function install() {
 }
 
 function unzipModules() {
-	if(isEmpty(downloaded)) {
+	if (isEmpty(downloaded)) {
 		install();
-	}
-	else {
+	} else {
 		var key;
-		for(key in downloaded) {
-			unpacked.push(key+suffix);
+		for (key in downloaded) {
+			unpacked.push(key + suffix);
 		}
 
 		unzipModule(unpacked, 0);
@@ -154,37 +166,39 @@ function unzipModules() {
 }
 
 function unzipModule(keys, idx) {
-	if(idx >= keys.length) { install(); return; }
+	if (idx >= keys.length) {
+		install();
+		return;
+	}
 
 	var mod = keys[idx];
-	if(mod.indexOf(".tar.gz") >= 0) {
+	if (mod.indexOf(".tar.gz") >= 0) {
 		var modDir = path.join("node_modules", mod.substring(0, mod.indexOf(suffix)));
-		if(fileExistsSync(modDir)) {
+		if (fileExistsSync(modDir)) {
 			rmdirSync(modDir);
 		}
 
-		if(platform === "win") {
+		if (platform === "win") {
 			exec("7z x " + mod, {cwd: "node_modules"}, function(error1, stdout1, stderr1) {
-				if(error1) throw error1;
+				if (error1) throw error1;
 
 				exec("7z x " + path.basename(mod, ".gz"), {cwd: "node_modules"}, function(error2, stdout2, stderr2) {
-					if(error2) throw error2;
+					if (error2) throw error2;
 
 					fs.unlinkSync(path.join("node_modules", path.basename(mod, ".gz")));
 					fs.unlinkSync(path.join("node_modules", mod));
 					unpacked[mod] = true;
 
-					unzipModule(keys, idx+1);
+					unzipModule(keys, idx + 1);
 				});
 			});
-		}
-		else {
+		} else {
 			exec("tar xzf " + mod, {cwd: "node_modules"}, function(error, stdout, stderr) {
-				if(error) throw error;
+				if (error) throw error;
 				fs.unlinkSync(path.join("node_modules", mod));
 				unpacked[mod] = true;
 
-				unzipModule(keys, idx+1);
+				unzipModule(keys, idx + 1);
 			});
 		}
 	}
@@ -200,16 +214,15 @@ function request(options, secure, callback) {
 
 			if (location.protocol === "http:") isSecure = false;
 			else isSecure = true;
-			request({host: location.host, path: location.pathname+location.search}, isSecure, callback);
-		}
-		else {
+			request({host: location.host, path: location.pathname + location.search}, isSecure, callback);
+		} else {
 			callback(res);
 		}
 	};
 
 	var req;
 	if (secure) req = https.get(options, responseCallback);
-	else        req = http.get(options, responseCallback);
+	else req = http.get(options, responseCallback);
 	req.on('error', function(e) {
 		console.log('problem with request: ' + e.message);
 	});
@@ -217,8 +230,8 @@ function request(options, secure, callback) {
 
 function allTrueDict(dict) {
 	var key;
-	for(key in dict) {
-		if(dict[key] !== true) return false;
+	for (key in dict) {
+		if (dict[key] !== true) return false;
 	}
 	return true;
 }
@@ -227,31 +240,30 @@ function isEmpty(obj) {
 	// null and undefined are "empty"
 	if (obj === null) return true;
 
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
+	// Assume if it has a length property with a non-zero value
+	// that that property is correct.
+	if (obj.length > 0)    return false;
+	if (obj.length === 0)  return true;
 
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
+	// Otherwise, does it have any properties of its own?
+	// Note that this doesn't handle
+	// toString and valueOf enumeration bugs in IE < 9
+	for (var key in obj) {
 		if (hasOwnProperty.call(obj, key)) return false;
-    }
+	}
 
-    return true;
+	return true;
 }
 
 function rmdirSync(directory) {
-	if(!fileExistsSync(directory) || !fs.lstatSync(directory).isDirectory()) return false;
+	if (!fileExistsSync(directory) || !fs.lstatSync(directory).isDirectory()) return false;
 
 	var list = fs.readdirSync(directory);
-	for (var j=0; j <list.length; j++) {
+	for (var j = 0; j < list.length; j++) {
 		var file = path.join(directory, list[j]);
 		if (fs.lstatSync(file).isDirectory()) {
 			rmdirSync(file);
-		}
-		else {
+		} else {
 			fs.unlinkSync(file);
 		}
 	}
