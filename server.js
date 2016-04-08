d485d4b26d2e5dedca001a2fadbfe6cd3106badb
Luc Renambot <renambot@uic.edu>
2016-04-07 20:16:12 -0500

    Revert "Fix media_stream sharing (desktop sharing): using port instead of secure port in id calculation"
    This reverts commit 5797f8ee62daa15ba556c46904610e666b6b01f2.

--- server.js
+++ server.js
@@ -2732,9 +2732,7 @@ function wsRequestNextRemoteFrame(wsio, data) {
 	} else {
 		originId = data.id;
 	}
-	// Luc here: changing to unsecure port since websocket communication to display is unsecure
-	// var remote_id = config.host + ":" + config.secure_port + "|" + data.id;
-	var remote_id = config.host + ":" + config.port + "|" + data.id;
+	var remote_id = config.host + ":" + config.secure_port + "|" + data.id;
 
 	if (SAGE2Items.applications.list.hasOwnProperty(originId)) {
 		var stream = SAGE2Items.applications.list[originId];
