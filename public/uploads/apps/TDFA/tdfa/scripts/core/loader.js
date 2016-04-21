161176734cc33bdd520696945fa6189ac14fd5bc
sharkgoesmad <sharkgoesmad@gmail.com>
2016-04-20 22:52:52 -0500

    Stub out custom loading manager for future use with trapping upload errors

--- public/uploads/apps/TDFA/tdfa/scripts/core/loader.js
+++ public/uploads/apps/TDFA/tdfa/scripts/core/loader.js
@@ -16,6 +16,8 @@ var modules =
 define(modules, function () {
 	"use strict";
 
+	var _oldTHREELoadingManager;
+
 	var _dae,
 		_fbx,
 		_mtl,
@@ -25,12 +27,25 @@ define(modules, function () {
 
 	function init() {
 
+		_oldTHREELoadingManager = THREE.DefaultLoadingManager;
+		THREE.DefaultLoadingManager = getLoadingManager();	// register our own
+
 		_dae = new THREE.ColladaLoader();
 		_fbx = new THREE.FBXLoader();
 		_mtl = new THREE.MTLLoader();
 		_obj = new THREE.OBJLoader();
 		_stl = new THREE.STLLoader();
 
+	}
+
+
+	function onLoadFailed(arg) {
+		debugger;
+	}
+
+
+	function onProgress(arg) {
+		debugger;
 	}
 
 
@@ -50,15 +65,30 @@ define(modules, function () {
 	}
 
 
+
+	function getLoadingManager() {
+
+		var _onLoad = function(url, itemsLoaded, itemsTotal) {
+		}
+
+		var _onProgress = function(url, itemsLoaded, itemsTotal) {
+		}
+
+		var _onError = function(url, itemsLoaded, itemsTotal) {
+		}
+
+		var _self = new THREE.LoadingManager(_onLoad, _onProgress, _onError);
+
+		return _self;
+	}
+
+
+
 	function load(uri, cb) {
 
-		var dotIdx,
-			extension,
-			loader;
-
-
-		dotIdx = uri.lastIndexOf(".");
-		extension = uri.substring(dotIdx + 1);
+		var dotIdx = uri.lastIndexOf("."),
+			extension = uri.substring(dotIdx + 1),
+			loader = null;
 
 
 		switch (extension) {
@@ -89,7 +119,13 @@ define(modules, function () {
 		}
 
 		if (loader !== null) {
-			loader.load(uri, getOnLoadedHandler(cb));
+			try {
+
+				loader.load(uri, getOnLoadedHandler(cb), onProgress, onLoadFailed);
+
+			} catch (error) {;
+				console.error(error);
+			}
 		}
 
 	}
