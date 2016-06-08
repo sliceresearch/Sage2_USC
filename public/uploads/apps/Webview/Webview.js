af7b86799225f21a27e4dc9ec3e489edbf29eb0e
Luc Renambot <renambot@gmail.com>
2016-06-07 19:18:57 -0500

    Implement zoom inside webview

--- public/uploads/apps/Webview/Webview.js
+++ public/uploads/apps/Webview/Webview.js
@@ -36,13 +36,17 @@ var Webview = SAGE2_App.extend({
 		this.element.minwidth  = data.width;
 		this.element.minheight = data.height;
 
-		this.element.src = this.state.url;
+		// this.element.src = data.params || this.state.url;
+
 		// this.element.src = "https://nytimes.com";
 		// this.element.src = "http://bl.ocks.org/mbostock/4060366";
 		// this.element.src = "https://docs.google.com/document/u/0/";
 		// this.element.src = "http://www.asquare.net/javascript/tests/KeyCode.html";
 		// this.element.src = "http://en.key-test.ru/";
 		// this.element.src = "https://maps.google.com/";
+		// this.element.src = "http://localhost:9292/emperor/index.html";
+		// this.element.src = "https://www.evl.uic.edu/aej/TEMPS/megamap.html";
+		this.element.src = "https://news.google.com";
 
 		this.zoomFactor = 1;
 		this.element.setZoomFactor(this.zoomFactor);
@@ -50,11 +54,12 @@ var Webview = SAGE2_App.extend({
 		var _this = this;
 		this.element.addEventListener("did-start-loading", function() {
 			console.log('Second loading...');
-			var content = _this.element.getWebContents();
-			content.enableDeviceEmulation({
-				screenPosition: "mobile",
+			_this.element.setZoomFactor(_this.zoomFactor);
+			// var content = _this.element.getWebContents();
+			// content.enableDeviceEmulation({
+				// screenPosition: "mobile",
 				// fitToView: true
-			});
+			// });
 		});
 
 
@@ -90,6 +95,56 @@ var Webview = SAGE2_App.extend({
 
 	quit: function() {
 		// Make sure to delete stuff (timers, ...)
+	},
+
+	getContextEntries: function() {
+		var entries = [];
+		var entry;
+
+		entry = {};
+		entry.description = "Zoom in";
+		entry.callback = "zoomPage";
+		entry.parameters = {};
+		entry.parameters.dir = "zoomin";
+		entries.push(entry);
+
+		entry = {};
+		entry.description = "Zoom out";
+		entry.callback = "zoomPage";
+		entry.parameters = {};
+		entry.parameters.dir = "zoomout";
+		entries.push(entry);
+
+		entry = {};
+		entry.description = "Reload";
+		entry.callback = "reloadPage";
+		entry.parameters = {};
+		entries.push(entry);
+
+		return entries;
+	},
+
+	reloadPage: function(responseObject) {
+		this.element.reload();
+		this.element.setZoomFactor(this.zoomFactor);
+	},
+
+	zoomPage: function(responseObject) {
+		var dir = responseObject.dir;
+
+		// zoomin
+		if (dir === "zoomin") {
+			this.zoomFactor *= 1.5;
+			this.element.setZoomFactor(this.zoomFactor);
+		}
+
+		// zoomout
+		if (dir === "zoomout") {
+			this.zoomFactor /= 1.5;
+			this.element.setZoomFactor(this.zoomFactor);
+		}
+		console.log('Zoom', this.zoomFactor);
+		this.refresh(this.prevDate);
 	},
 
 	event: function(eventType, position, user_id, data, date) {
