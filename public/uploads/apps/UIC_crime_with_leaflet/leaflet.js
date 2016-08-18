44c3208d94ae30d8e3f8c0fd2d8d06eb18c94daa
Luc Renambot <renambot@uic.edu>
2016-08-18 12:46:10 -0500

    linting

--- public/uploads/apps/UIC_crime_with_leaflet/leaflet.js
+++ public/uploads/apps/UIC_crime_with_leaflet/leaflet.js
@@ -280,12 +280,31 @@ var leaflet = SAGE2_App.extend({
 			.data(collection)
 			.enter()
 			.append("svg:circle")
-			.style("stroke", function(d) { if (d.inLastMonth) { return "black"; } else { return "white"; } })
-			.style("stroke-width", function(d) { if (d.inLastMonth) { return 4; } else { return 2; } })
-			.style("opacity", function(d) { if (d.inLastMonth) { return 1.0; } else { return 0.4; } })
-			.style("fill", function(d) { return d.color; })
-			.attr("r", circleSize); // 15
-
+			.style("stroke", function(d) {
+				if (d.inLastMonth) {
+					return "black";
+				} else {
+					return "white";
+				}
+			})
+			.style("stroke-width", function(d) {
+				if (d.inLastMonth) {
+					return 4;
+				} else {
+					return 2;
+				}
+			})
+			.style("opacity", function(d) {
+				if (d.inLastMonth) {
+					return 1.0;
+				} else {
+					return 0.4;
+				}
+			})
+			.style("fill", function(d) {
+				return d.color;
+			})
+			.attr("r", circleSize);
 		/* eslint-enable brace-style */
 
 		var myStrokeColor;
