8f3456971192184352e24b4d71b9a892290d6540
d-k-k <dylank@hawaii.edu>
2016-08-29 19:09:28 -1000

    reduced

--- public/src/AppControl_UI.js
+++ public/src/AppControl_UI.js
@@ -24,30 +24,8 @@ navigator.getUserMedia   = (navigator.getUserMedia  || navigator.webkitGetUserMe
 document.exitPointerLock = document.exitPointerLock ||
 							document.mozExitPointerLock  ||
 							document.webkitExitPointerLock;
+var wsio;
 
-//
-// Polyfill for 'bind' - needed for older version of iOS Safari mobile ;-(
-//
-/* eslint-disable */
-if (!Function.prototype.bind) {
-	Function.prototype.bind = function(oThis) {
-		if (typeof this !== 'function') {
-			// closest thing possible to the ECMAScript 5
-			// internal IsCallable function
-			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-		}
-		var aArgs = Array.prototype.slice.call(arguments, 1);
-		var _this = this;
-		var FNOP    = function() {};
-		var fBound  = function() {
-			return _this.apply(this instanceof FNOP && oThis ? this : oThis,
-						aArgs.concat(Array.prototype.slice.call(arguments)));
-		};
-		FNOP.prototype = this.prototype;
-		fBound.prototype = new FNOP();
-		return fBound;
-	};
-}
 
 
 //
@@ -61,8 +39,6 @@ if (!String.prototype.startsWith) {
 }
 /* eslint-enable */
 //
-
-var wsio;
 
 
 /**
