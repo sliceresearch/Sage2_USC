1de4ff6d6c6d3bb5e8c49a5d0bc1d472f541c172
jauri <jillian.aurisano@gmail.com>
2016-09-27 11:32:16 -0600

    tracks and logs to console if left finger is pointing forward and up

--- public/uploads/apps/skeletonTracker/skeleton.js
+++ public/uploads/apps/skeletonTracker/skeleton.js
@@ -5,6 +5,24 @@
 // Copyright (c) 2015
 //
 
+function handIsForward (hand, shoulder) {
+	if (hand.z + 0.15 < shoulder.z) {
+		return true;
+	} else return false;
+}
+
+function handIsRaised (hand, shoulder) {
+	if (hand.y + 100 < shoulder.y) {
+		return true;
+	} else return false;
+}
+
+function fingerIsPointing (fingerTip, hand) {
+	if (fingerTip.z + 0.02 < hand.z) {
+		return true;
+	} else return false;
+}
+
 var skeleton = SAGE2_App.extend( {
 	init: function(data) {
 		// Create div into the DOM
@@ -36,7 +54,6 @@ var skeleton = SAGE2_App.extend( {
 	load: function(date) {
 		console.log('articulate_ui> Load with state value', this.state.value);
 
-
 		this.refresh(date);
 	},
 
@@ -63,29 +80,46 @@ var skeleton = SAGE2_App.extend( {
 			this.ctx.textAlign="center";
 
 			//status bar
-			this.ctx.fillStyle = "rgba(255, 255, 255, 1.0)";
+			this.ctx.fillStyle = "white";
 			this.ctx.fillText( "Input: " + this.textToDraw, this.element.width/2.0, 32);
 
-			this.ctx.fillStyle = skeleton.color || this.ctx.fillStyle;
+			this.ctx.fillStyle = skeleton.color;
 			this.ctx.fillText("head", skeleton.headPosition.x, skeleton.headPosition.y);
 			this.ctx.fillText("leftHand", skeleton.leftHandPosition.x, skeleton.leftHandPosition.y);
 			this.ctx.fillText("rightHand", skeleton.rightHandPosition.x, skeleton.rightHandPosition.y);
 			this.ctx.fillText(skeletonID, skeleton.shoulderCenterPosition.x, skeleton.shoulderCenterPosition.y);
 
-			this.ctx.beginPath();
-			this.ctx.arc(skeleton.headPosition.x, skeleton.headPosition.y, 15, 0, 2*Math.PI)
-			this.ctx.fill();
-			this.ctx.stroke();
+			for (const bodyPartName in skeleton) {
+				const bodyPart = skeleton[bodyPartName];
 
-			this.ctx.beginPath();
-			this.ctx.arc(skeleton.leftHandPosition.x, skeleton.leftHandPosition.y, 10, 0, 2*Math.PI)
-			this.ctx.fill();
-			this.ctx.stroke();
+				const shape = bodyPart.shape;
+				const x = bodyPart.x;
+				const y = bodyPart.y;
+				const z = bodyPart.z;
+				const size = bodyPart.baseSize / z;
 
-			this.ctx.beginPath();
-			this.ctx.arc(skeleton.rightHandPosition.x, skeleton.rightHandPosition.y, 10, 0, 2*Math.PI)
-			this.ctx.fill();
-			this.ctx.stroke();
+				if (shape === "circle") {
+					this.ctx.beginPath();
+					this.ctx.arc(x, y, size, 0, 2*Math.PI);
+					this.ctx.fill();
+					this.ctx.stroke();
+				} else if (shape === "square") {
+					this.ctx.rect(x, y, size, size);
+					this.ctx.fill();
+					this.ctx.stroke();
+				}
+			}
+
+			const leftFingerTip = skeleton.leftFingerTipPosition;
+			const leftHand = skeleton.leftHandPosition;
+			const leftShoulder = skeleton.leftShoulderPosition;
+
+			if (handIsForward(leftHand, leftShoulder) &&
+				handIsRaised(leftHand, leftShoulder) &&
+				fingerIsPointing(leftFingerTip, leftHand)) {
+				console.log("Finger is pointing foward and up");
+			}
+
 		}
 	},
 
@@ -109,9 +143,9 @@ var skeleton = SAGE2_App.extend( {
 	//--------------EVENT FUNCTIONS-------------//
 	//------------------------------------------//
 	event: function(eventType, position, user_id, data, date) {
-		const skeletonColors = ["red", "blue", "green", "orange", "pink", "white"];
+		const skeletonColors = ["red", "blue", "green", "orange", "pink"];
 
-		if( eventType == "kinectInput"){
+		if( eventType === "kinectInput"){
 			const skeletonID = data["skeletonID"];
 
 			if (!this.skeletons[skeletonID]) {
@@ -120,37 +154,75 @@ var skeleton = SAGE2_App.extend( {
 				for (var i = 0; i < skeletonColors.length; i++) {
 					const skeletonColor = skeletonColors[i];
 					const skeletonsWithThisColor = _.pickBy(this.skeletons, function (skeleton) {
-						return skeleton.color == skeletonColor;
+						return skeleton.color === skeletonColor;
 					});
 
-					if (_.size(skeletonsWithThisColor) == 0) {
+					if (_.size(skeletonsWithThisColor) === 0) {
 						availableColors.push(skeletonColor);
 					}
-				};
+				}
 
 				this.skeletons[skeletonID] = {
-					headPosition: {},
-					leftHandPosition: {},
-					rightHandPosition: {},
-					shoulderCenterPosition: {},
-					color: availableColors.length > 0 ? _.shuffle(availableColors)[0] : undefined
+					color: availableColors.length > 0 ? _.shuffle(availableColors)[0] : "white"
 				};
 			}
 
-			const headPos = data["OMICRON_SKEL_HEAD"];
-			const leftHandPos = data["OMICRON_SKEL_LEFT_HAND"];
-			const rightHandPos = data["OMICRON_SKEL_RIGHT_HAND"];
-			const shoulderCenterPos = data["OMICRON_SKEL_SHOULDER_CENTER"];
+			const bodyParts = {
+				"OMICRON_SKEL_HEAD": {
+					"partName": "headPosition",
+					"shape": "circle",
+					"baseSize": 20
+				},
+				"OMICRON_SKEL_LEFT_HAND": {
+					"partName": "leftHandPosition",
+					"shape": "square",
+					"baseSize": 15
+				},
+				"OMICRON_SKEL_RIGHT_HAND": {
+					"partName": "rightHandPosition",
+					"shape": "square",
+					"baseSize": 15
+				},
+				"OMICRON_SKEL_SHOULDER_CENTER": {
+					"partName": "shoulderCenterPosition",
+					"shape": "square",
+					"baseSize": 5
+				},
+				"OMICRON_SKEL_LEFT_SHOULDER": {
+					"partName": "leftShoulderPosition",
+					"shape": "circle",
+					"baseSize": 10
+				},
+				"OMICRON_SKEL_RIGHT_SHOULDER": {
+					"partName": "rightShoulderPosition",
+					"shape": "circle",
+					"baseSize": 10
+				},
+				"OMICRON_SKEL_LEFT_FINGERTIP": {
+					"partName": "leftFingerTipPosition",
+					"shape": "circle",
+					"baseSize": "2"
+				},
+				"OMICRON_SKEL_RIGHT_FINGERTIP": {
+					"partName": "rightFingerTipPosition",
+					"shape": "circle",
+					"baseSize": "2"
+				}
+			};
+			for (const bodyPartID in data) {
+				const bodyPart = data[bodyPartID];
 
-			this.skeletons[skeletonID].headPosition.x = this.element.width/2.0+headPos.x*this.element.width;
-			this.skeletons[skeletonID].headPosition.y = this.element.height/2.0-headPos.y*this.element.height;
-			this.skeletons[skeletonID].leftHandPosition.x = this.element.width/2.0+leftHandPos.x*this.element.width;
-			this.skeletons[skeletonID].leftHandPosition.y = this.element.height/2.0-leftHandPos.y*this.element.height;
-			this.skeletons[skeletonID].rightHandPosition.x = this.element.width/2.0+rightHandPos.x*this.element.width;
-			this.skeletons[skeletonID].rightHandPosition.y = this.element.height/2.0-rightHandPos.y*this.element.height;
-			this.skeletons[skeletonID].shoulderCenterPosition.x = this.element.width/2.0+shoulderCenterPos.x*this.element.width;
-			this.skeletons[skeletonID].shoulderCenterPosition.y = this.element.height/2.0-shoulderCenterPos.y*this.element.height;
+				if (bodyParts[bodyPartID]) {
+					const bodyPartName = bodyParts[bodyPartID].partName;
 
+					this.skeletons[skeletonID][bodyPartName] = {};
+					this.skeletons[skeletonID][bodyPartName].x = this.element.width/2.0+bodyPart.x*this.element.width;
+					this.skeletons[skeletonID][bodyPartName].y = this.element.height/2.0-bodyPart.y*this.element.height;
+					this.skeletons[skeletonID][bodyPartName].z = bodyPart.z/2.0;
+					this.skeletons[skeletonID][bodyPartName].shape = bodyParts[bodyPartID].shape;
+					this.skeletons[skeletonID][bodyPartName].baseSize = bodyParts[bodyPartID].baseSize;
+				}
+			}
 			this.skeletons[skeletonID].lastUpdate = date.getTime();
 
 			this.inputCount++;
