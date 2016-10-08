f91fb06a4cf727613da2b3e5add39e1102a30a94
Luc Renambot <renambot@uic.edu>
2016-10-07 20:28:58 -0500

    Update apps needing the Shift key detected.

--- public/uploads/apps/zoom/zoom.js
+++ public/uploads/apps/zoom/zoom.js
@@ -18,19 +18,16 @@ var zoom = SAGE2_App.extend({
 
 		this.resizeEvents = "continuous";
 
-		this.lastZoom  = null;
-		this.lastClick = null;
-		this.isShift   = null;
-		this.dragging  = null;
-		this.position  = null;
-
 		this.element.id = "div" + data.id;
 		this.element.style.background = "black";
+
 		this.lastZoom  = data.date;
 		this.lastClick = data.date;
 		this.dragging  = false;
 		this.isShift   = false;
 		this.position  = {x: 0, y: 0};
+
+		this.scrollAmount = 0;
 
 		// create the image viewer with the right data and path
 		this.viewer = OpenSeadragon({
@@ -89,9 +86,7 @@ var zoom = SAGE2_App.extend({
 			this.lastClick  = date;
 		} else if (eventType === "pointerMove" && this.dragging) {
 			var delta = new OpenSeadragon.Point(this.position.x - position.x, this.position.y - position.y);
-			this.viewer.viewport.panBy(
-			this.viewer.viewport.deltaPointsFromPixels(delta)
-			);
+			this.viewer.viewport.panBy(this.viewer.viewport.deltaPointsFromPixels(delta));
 			this.position.x = position.x;
 			this.position.y = position.y;
 		} else if (eventType === "pointerRelease" && (data.button === "left")) {
@@ -100,49 +95,43 @@ var zoom = SAGE2_App.extend({
 			this.position.y = position.y;
 		} else if (eventType === "pointerScroll") {
 			// Scroll events for zoom
-			var amount = data.wheelDelta;
-			var diff = date - this.lastZoom;
-			if (amount >= 1 && (diff > 300)) {
-				// zoom in
+			this.scrollAmount += data.wheelDelta;
+
+			if (this.scrollAmount >= 24) {
+				// zoom out
 				this.viewer.viewport.zoomBy(0.8);
 				this.viewer.viewport.applyConstraints();
-				this.lastZoom = date;
-			} else if (amount <= 1 && (diff > 300)) {
-				// zoom out
+				this.scrollAmount = 0;
+			} else if (this.scrollAmount <= -24) {
+				// zoom in
 				this.viewer.viewport.zoomBy(1.2);
 				this.viewer.viewport.applyConstraints();
-				this.lastZoom = date;
+				this.scrollAmount = 0;
 			}
-		} else if (eventType === "specialKey" && data.code === 16 && data.state === "down") {
-			// shift down
-			this.isShift = true;
-			// zoom in
-			// this.viewer.viewport.zoomBy(1.1);
-			// this.viewer.viewport.applyConstraints();
-		} else if (eventType === "specialKey" && data.code === 16 && data.state === "up") {
-			// shift up
-			this.isShift = false;
-		} else if (eventType === "specialKey" && data.code === 17 && data.state === "down") {
-			// control down
-			// zoom out
-			// this.viewer.viewport.zoomBy(0.9);
-			// this.viewer.viewport.applyConstraints();
-		} else if (eventType === "specialKey" && data.code === 37 && data.state === "down") {
-			// left
-			this.viewer.viewport.panBy(new OpenSeadragon.Point(-0.01, 0));
-			this.viewer.viewport.applyConstraints();
-		} else if (eventType === "specialKey" && data.code === 38 && data.state === "down") {
-			// up
-			this.viewer.viewport.panBy(new OpenSeadragon.Point(0, -0.01));
-			this.viewer.viewport.applyConstraints();
-		} else if (eventType === "specialKey" && data.code === 39 && data.state === "down") {
-			// right
-			this.viewer.viewport.panBy(new OpenSeadragon.Point(0.01, 0));
-			this.viewer.viewport.applyConstraints();
-		} else if (eventType === "specialKey" && data.code === 40 && data.state === "down") {
-			// down
-			this.viewer.viewport.panBy(new OpenSeadragon.Point(0, 0.01));
-			this.viewer.viewport.applyConstraints();
+		} else if (eventType === "specialKey") {
+			if (data.status && data.status.SHIFT) {
+				// Shift key
+				this.isShift = true;
+			} else {
+				this.isShift = false;
+			}
+			if (data.code === 37 && data.state === "down") {
+				// left
+				this.viewer.viewport.panBy(new OpenSeadragon.Point(-0.01, 0));
+				this.viewer.viewport.applyConstraints();
+			} else if (data.code === 38 && data.state === "down") {
+				// up
+				this.viewer.viewport.panBy(new OpenSeadragon.Point(0, -0.01));
+				this.viewer.viewport.applyConstraints();
+			} else if (data.code === 39 && data.state === "down") {
+				// right
+				this.viewer.viewport.panBy(new OpenSeadragon.Point(0.01, 0));
+				this.viewer.viewport.applyConstraints();
+			} else if (data.code === 40 && data.state === "down") {
+				// down
+				this.viewer.viewport.panBy(new OpenSeadragon.Point(0, 0.01));
+				this.viewer.viewport.applyConstraints();
+			}
 		} else if (eventType === "widgetEvent") {
 			switch (data.identifier) {
 				case "Up":
