e3ca74ac5fdbf55f666239dc3ae61c90e921784f
Luc Renambot <renambot@uic.edu>
2017-01-23 14:01:56 -0600

    Move copyToClipboard function to the SAGE2 runtime file: SAGE2_copyToClipboard

--- public/src/SAGE2_fileManager.js
+++ public/src/SAGE2_fileManager.js
@@ -1057,42 +1057,11 @@ function FileManager(wsio, mydiv, uniqueID) {
 		}
 	}
 
-	// From stackoverflow:
-	// Copies a string to the clipboard. Must be called from within an
-	// event handler such as click. May return false if it failed, but
-	// this is not always possible. Browser support for Chrome 43+,
-	// Firefox 42+, Safari 10+, Edge and IE 10+.
-	// IE: The clipboard feature may be disabled by an administrator. By
-	// default a prompt is shown the first time the clipboard is
-	// used (per session).
-	function copyToClipboard(text) {
-		if (window.clipboardData && window.clipboardData.setData) {
-			// IE specific code path to prevent textarea being shown while dialog is visible.
-			return window.clipboardData.setData("Text", text);
-		} else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-			var textarea = document.createElement("textarea");
-			textarea.textContent = text;
-			// Prevent scrolling to bottom of page in MS Edge
-			textarea.style.position = "fixed";
-			document.body.appendChild(textarea);
-			textarea.select();
-			try {
-				// Security exception may be thrown by some browsers
-				return document.execCommand("copy");
-			} catch (ex) {
-				console.warn("Copy to clipboard failed.", ex);
-				return false;
-			} finally {
-				document.body.removeChild(textarea);
-			}
-		}
-	}
-
 	function copyURLItem(elt) {
 		var url = _this.allFiles[elt].sage2URL;
 		if (url) {
-			// Copy to clipboard
-			copyToClipboard(window.location.origin + url);
+			// Copy to clipboard (defined in SAGE2_runtime)
+			SAGE2_copyToClipboard(window.location.origin + url);
 		}
 	}
 
