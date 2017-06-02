6f2e62d736484667a1eeef1eb7d6066e7e1dd3ef
D K <dylank@hawaii.edu>
2017-06-01 21:55:47 -1000

    fix: fixes issue #96 by having sabi launch using the host in the config file. This will require a recopy of the launch script. Easiest way to do this is delete the SAGE2_Media\sabiConfig folder and relaunch sabi to have it copy the files over automatically. The bat file was additive, so it will not break previous installations if they upgrade. But the image download error will remain until the bat file gets replaced.

--- sabi.js/scripts/s2_on_electron.bat
+++ sabi.js/scripts/s2_on_electron.bat
@@ -9,6 +9,7 @@ rem %2 index_port, NOT https
 rem %3 width
 rem %4 height
 rem %5 hash
+rem %6 host in the config file, added here to allow for backwards compatibility
 
 rem The above are necessary so this doesn't need to be edited
 
@@ -18,12 +19,12 @@ rem If you want to make a custom launcher look at sage2_on.bat
 
 start /MIN /D .. sage2.bat -f %1
 rem delay about 2 seconds
-ping localhost -n 2
+ping localhost -n 3
 
 rem audio manager
-start "Electron" /MIN electron.bat -s http://localhost:%2 -a --hash %5
+start "Electron" /MIN electron.bat -s http://%6:%2 -a --hash %5
 rem delay about 2 seconds
 ping localhost -n 2
 
 rem display
-start "Electron" /MIN electron.bat -s http://localhost:%2 -d 0 -n -x 0 -y 0 --width %3 --height %4 --hash %5
+start "Electron" /MIN electron.bat -s http://%6:%2 -d 0 -n -x 0 -y 0 --width %3 --height %4 --hash %5
