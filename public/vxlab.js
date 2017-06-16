d3f4d9e0a857ef251159c49d62be3b21f804ac81
Ian Peake <ian.peake@rmit.edu.au>
2017-06-16 17:15:35 +1000

    support vnc password in custom UI

--- public/vxlab.js
+++ public/vxlab.js
@@ -27,15 +27,23 @@ function loadWebURL(){
 function vncURL(){
   var display_value = document.getElementById("req_url").value;
   var port_value = 0;
+  console.log("display_value "+display_value);
+  var pwdPos = display_value.indexOf("@");
+  console.log("pwdPos",pwdPos);
+  if (pwdPos>-1) {
+    pwd_value = display_value.substr(0,pwdPos);
+    display_value = display_value.substr(pwdPos+1,display_value.length);
+    console.log("pwd_value",pwd_value);
+  }
   var n = display_value.indexOf(":");
-  console.log("d "+display_value);
   if (n>-1) {
     port_value = display_value.substr(n+1,display_value.length);
     display_value = display_value.substr(0,n);
   }
-  console.log("d "+display_value);
-  console.log("p "+port_value);
-  var sjdurl =  "https://10.234.2.30:8086/sage-vnc-ipaddr?"+display_value+ "?" + port_value;
+  console.log("display "+display_value);
+  console.log("port "+port_value);
+  var sjdurl =  "https://10.234.2.30:8086/sage-vnc-ipaddr?"+display_value+ "?" + port_value+"?"+pwd_value;
+  console.log("url "+sjdurl);
   //popup = window.open(sjdurl,"popup"," menubar =0,toolbar =0,location=0, height=900, width=1000");
   //popup.window.moveTo(950,150);
   httpGetAsync(sjdurl);
