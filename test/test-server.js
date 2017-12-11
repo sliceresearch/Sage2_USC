3767955c2f0d47b81163e5594484066f27d94abf
Levi Kotzur <lkotzur@usc.edu.au>
2017-12-11 16:12:58 +1000

    Testing

--- test/test-server.js
+++ test/test-server.js
@@ -1,14 +1,26 @@
+/**
+ * SLICE
+ * Use Node cmd to run mocha.
+ */
 'use strict';
 
+// mocha.setup('bdd');
+
 var assert = require('assert');
+var sage2;
 
 describe('Running server.js', function(done) {
-    this.timeout(15000);
+    this.timeout(20000);
     it('Running sage2...', function(done) {
-        setTimeout(done, 15000);
-        var sage2 = require('../server');
-        done();
+        sage2 = require('../server');
+        var wsioServer  = new WebsocketIO.Server({server: sage2Server});
+        wsioServer.onconnection(openWebSocketClient);
+        setTimeout(done, 20000);
+        this.interval = setInterval(function () {
+			sage2.wsio.on('loadApplication', {application: "D:\\Sage2_USC\\public\\uploads\\apps\\chronicles_of_spaceman_spiff", user: "127.0.0.1:62031"});
+            done();
+		}, 10000);
+        
     });
-
 });
 
